7ad5c1e28dfc02037a2d0072344c4e68900e3669
Andy Yu <shaoyu@microsoft.com>
2016-08-24 11:55:06 -0700

    init perf test

---
+++ test/Microsoft.SqlTools.ServiceLayer.PerformanceTest/Utility/TestObjects.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.SqlTools.ServiceLayer.Connection;
+using Microsoft.SqlTools.ServiceLayer.Connection.Contracts;
+using Microsoft.SqlTools.ServiceLayer.LanguageServices;
+using Microsoft.SqlTools.ServiceLayer.SqlContext;
+using Microsoft.SqlTools.ServiceLayer.PerformanceTest.Utility;
+
+namespace Microsoft.SqlTools.ServiceLayer.PerformanceTest.Utility
+{
+    /// <summary>
+    /// Tests for the ServiceHost Connection Service tests
+    /// </summary>
+    public class TestObjects
+    {
+        /// <summary>
+        /// Creates a test connection service
+        /// </summary>
+        public static ConnectionService GetTestConnectionService()
+        {
+#if !USE_LIVE_CONNECTION
+            // use mock database connection
+            return new ConnectionService(new TestSqlConnectionFactory());
+#else
+            // connect to a real server instance
+            return ConnectionService.Instance;
+#endif
+        }
+
+        public static ConnectParams GetTestConnectionParams()
+        {
+            return new ConnectParams()
+            {
+                OwnerUri = "file://some/file.sql",
+                Connection = GetTestConnectionDetails()
+            };
+        }
+
+        /// <summary>
+        /// Creates a test connection details object
+        /// </summary>
+        public static ConnectionDetails GetTestConnectionDetails()
+        {
+            return new ConnectionDetails()
+            {
+                UserName = "sa",
+                Password = "Yukon900",
+                DatabaseName = "AdventureWorks2016CTP3_2",
+                ServerName = "sqltools11"
+            };
+        }
+
+        /// <summary>
+        /// Create a test language service instance
+        /// </summary>
+        /// <returns></returns>
+        public static LanguageService GetTestLanguageService()
+        {
+            return new LanguageService();
+        }
+
+        /// <summary>
+        /// Creates a test autocomplete service instance
+        /// </summary>
+        public static AutoCompleteService GetAutoCompleteService()
+        {
+            return AutoCompleteService.Instance;
+        }
+
+        /// <summary>
+        /// Creates a test sql connection factory instance
+        /// </summary>
+        public static ISqlConnectionFactory GetTestSqlConnectionFactory()
+        {
+#if !USE_LIVE_CONNECTION
+            // use mock database connection
+            return new TestSqlConnectionFactory();
+#else
+            // connect to a real server instance
+            return ConnectionService.Instance.ConnectionFactory;
+#endif
+
+        }
+    }
+
+    /// <summary>
+    /// Test mock class for IDbCommand
+    /// </summary>
+    public class TestSqlCommand : DbCommand
+    {
+        internal TestSqlCommand(Dictionary<string, string>[][] data)
+        {
+            Data = data;
+        }
+
+        internal Dictionary<string, string>[][] Data { get; set; }
+
+        public override void Cancel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int ExecuteNonQuery()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ExecuteScalar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Prepare()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string CommandText { get; set; }
+        public override int CommandTimeout { get; set; }
+        public override CommandType CommandType { get; set; }
+        public override UpdateRowSource UpdatedRowSource { get; set; }
+        protected override DbConnection DbConnection { get; set; }
+        protected override DbParameterCollection DbParameterCollection { get; }
+        protected override DbTransaction DbTransaction { get; set; }
+        public override bool DesignTimeVisible { get; set; }
+
+        protected override DbParameter CreateDbParameter()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override DbDataReader ExecuteDbDataReader(CommandBehavior behavior)
+        {
+            return new TestDbDataReader(Data);
+        }
+    }
+
+    /// <summary>
+    /// Test mock class for SqlConnection wrapper
+    /// </summary>
+    public class TestSqlConnection : DbConnection
+    {
+        internal TestSqlConnection(Dictionary<string, string>[][] data)
+        {
+            Data = data;
+        }
+
+        internal Dictionary<string, string>[][] Data { get; set; }
+
+        protected override DbTransaction BeginDbTransaction(IsolationLevel isolationLevel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Close()
+        {
+            // No Op
+        }
+
+        public override void Open()
+        {
+            // No Op, unless credentials are bad
+            if (ConnectionString.Contains("invalidUsername"))
+            {
+                throw new Exception("Invalid credentials provided");
+            }
+        }
+
+        public override string ConnectionString { get; set; }
+        public override string Database { get; }
+        public override ConnectionState State { get; }
+        public override string DataSource { get; }
+        public override string ServerVersion { get; }
+
+        protected override DbCommand CreateDbCommand()
+        {
+            return new TestSqlCommand(Data);
+        }
+
+        public override void ChangeDatabase(string databaseName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// Test mock class for SqlConnection factory
+    /// </summary>
+    public class TestSqlConnectionFactory : ISqlConnectionFactory
+    {
+        public DbConnection CreateSqlConnection(string connectionString)
+        {
+            return new TestSqlConnection(null)
+            {
+                ConnectionString = connectionString
+            };
+        }
+    }
+}
