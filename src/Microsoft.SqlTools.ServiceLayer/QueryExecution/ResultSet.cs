ba7dd70894bee2a3cd5f8f9e2cfed90e59a52ea5
Sharon Ravindran <shravind@microsoft.com>
2016-10-12 17:22:45 -0700

    Remove toList

--- src/Microsoft.SqlTools.ServiceLayer/QueryExecution/ResultSet.cs
+++ src/Microsoft.SqlTools.ServiceLayer/QueryExecution/ResultSet.cs
@@ -169,8 +169,8 @@ namespace Microsoft.SqlTools.ServiceLayer.QueryExecution
                 if (isSingleColumnXmlJsonResultSet)
                 {
                     // Iterate over all the rows and process them into a list of string builders
-                    List<StringBuilder> sbRows = FileOffsets.Select(rowOffset => fileStreamReader.ReadRow(rowOffset, Columns)
-                        .Select(cell => cell.DisplayValue).Aggregate(new StringBuilder(), (sb, value) => sb.Append(value))).ToList();
+                    IEnumerable<StringBuilder> sbRows = FileOffsets.Select(rowOffset => fileStreamReader.ReadRow(rowOffset, Columns)
+                        .Select(cell => cell.DisplayValue).Aggregate(new StringBuilder(), (sb, value) => sb.Append(value)));
                     rows = new[] { new[] { string.Join(string.Empty, sbRows) } };
 
                 }
