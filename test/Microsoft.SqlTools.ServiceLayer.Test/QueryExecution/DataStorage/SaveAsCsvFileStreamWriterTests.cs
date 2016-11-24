29b237c2f77dde4a67e17502cdab78abff2613fc
Benjamin Russell <beruss@microsoft.com>
2016-11-23 17:38:48 -0800

    Adding unit tests for CSV encoding

---
+++ test/Microsoft.SqlTools.ServiceLayer.Test/QueryExecution/DataStorage/SaveAsCsvFileStreamWriterTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.SqlTools.ServiceLayer.QueryExecution.DataStorage;
+using Xunit;
+
+namespace Microsoft.SqlTools.ServiceLayer.Test.QueryExecution.DataStorage
+{
+    public class SaveAsCsvFileStreamWriterTests
+    {
+        [Theory]
+        [InlineData("Something\rElse")]
+        [InlineData("Something\nElse")]
+        [InlineData("Something\"Else")]
+        [InlineData("Something,Else")]
+        [InlineData("\tSomething")]
+        [InlineData("Something\t")]
+        [InlineData(" Something")]
+        [InlineData("Something ")]
+        [InlineData(" \t\r\n\",\r\n\"\r ")]
+        public void EncodeCsvFieldShouldWrap(string field)
+        {
+            // If: I CSV encode a field that has forbidden characters in it
+            string output = SaveAsCsvFileStreamWriter.EncodeCsvField(field);
+
+            // Then: It should wrap it in quotes
+            Assert.True(Regex.IsMatch(output, "^\".*")
+                && Regex.IsMatch(output, ".*\"$"));
+        }
+
+        [Theory]
+        [InlineData("Something")]
+        [InlineData("Something valid.")]
+        [InlineData("Something\tvalid")]
+        public void EncodeCsvFieldShouldNotWrap(string field)
+        {
+            // If: I CSV encode a field that does not have forbidden characters in it
+            string output = SaveAsCsvFileStreamWriter.EncodeCsvField(field);
+
+            // Then: It should not wrap it in quotes
+            Assert.False(Regex.IsMatch(output, "^\".*\"$"));
+        }
+
+        [Fact]
+        public void EncodeCsvFieldReplace()
+        {
+            // If: I CSV encode a field that has a double quote in it,
+            string output = SaveAsCsvFileStreamWriter.EncodeCsvField("Some\"thing");
+
+            // Then: It should be replaced with double double quotes
+            Assert.Equal("\"Some\"\"thing\"", output);
+        }
+        
+    }
+}
