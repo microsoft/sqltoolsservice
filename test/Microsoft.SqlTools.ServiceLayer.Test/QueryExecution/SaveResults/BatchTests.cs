6bd8506551445ad375ae4726037f588a65488e58
Benjamin Russell <beruss@microsoft.com>
2016-12-01 17:52:49 -0800

    Unit tests for batch/query argument in range

---
+++ test/Microsoft.SqlTools.ServiceLayer.Test/QueryExecution/SaveResults/BatchTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.SqlTools.ServiceLayer.QueryExecution;
+using Microsoft.SqlTools.ServiceLayer.QueryExecution.Contracts;
+using Xunit;
+
+namespace Microsoft.SqlTools.ServiceLayer.Test.QueryExecution.SaveResults
+{
+    public class BatchTests
+    {
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(100)]
+        public void SaveAsFailsOutOfRangeResultSet(int resultSetIndex)
+        {
+            // If: I attempt to save results for an invalid result set index
+            // Then: I should get an ArgumentOutOfRange exception
+            Batch batch = Common.GetBasicExecutedBatch();
+            SaveResultsRequestParams saveParams = new SaveResultsRequestParams {ResultSetIndex = resultSetIndex};
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+                batch.SaveAs(saveParams, null, null, null));
+        }
+    }
+}
---
+++ test/Microsoft.SqlTools.ServiceLayer.Test/QueryExecution/SaveResults/QueryTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.SqlTools.ServiceLayer.QueryExecution;
+using Microsoft.SqlTools.ServiceLayer.QueryExecution.Contracts;
+using Xunit;
+
+namespace Microsoft.SqlTools.ServiceLayer.Test.QueryExecution.SaveResults
+{
+    public class QueryTests
+    {
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(100)]
+        public void SaveAsFailsOutOfRangeBatch(int batchIndex)
+        {
+            // If: I save a basic query's results with out of range batch index
+            // Then: I should get an out of range exception
+            Query query = Common.GetBasicExecutedQuery();
+            SaveResultsRequestParams saveParams = new SaveResultsRequestParams {BatchIndex = batchIndex};
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+                query.SaveAs(saveParams, null, null, null));
+        }
+    }
+}
