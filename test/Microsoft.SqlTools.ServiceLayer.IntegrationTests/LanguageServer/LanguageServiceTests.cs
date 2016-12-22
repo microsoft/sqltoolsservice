8fdf981121dbe059fd06ef13a7f48214c6501587
Connor Quagliana <coquagli@microsoft.com>
2016-12-21 23:47:04 -0800

    Moved new LanguageService tests to IntegrationTest project

--- test/Microsoft.SqlTools.ServiceLayer.IntegrationTests/LanguageServer/LanguageServiceTests.cs
+++ test/Microsoft.SqlTools.ServiceLayer.IntegrationTests/LanguageServer/LanguageServiceTests.cs
@@ -19,191 +19,6 @@ namespace Microsoft.SqlTools.ServiceLayer.IntegrationTests.LanguageServer
     /// </summary>
     public class LanguageServiceTests
     {
-        #region "Diagnostics tests"
-
-
-        /// <summary>
-        /// Verify that the latest SqlParser (2016 as of this writing) is used by default
-        /// </summary>
-        [Fact]
-        public void LatestSqlParserIsUsedByDefault()
-        {
-            // This should only parse correctly on SQL server 2016 or newer
-            const string sql2016Text = 
-                @"CREATE SECURITY POLICY [FederatedSecurityPolicy]" + "\r\n" +
-                @"ADD FILTER PREDICATE [rls].[fn_securitypredicate]([CustomerId])" + "\r\n" +   
-                @"ON [dbo].[Customer];";
-            
-            LanguageService service = TestObjects.GetTestLanguageService();
-
-            // parse
-            var scriptFile = new ScriptFile();
-            scriptFile.SetFileContents(sql2016Text);
-            ScriptFileMarker[] fileMarkers = service.GetSemanticMarkers(scriptFile);
-
-            // verify that no errors are detected
-            Assert.Equal(0, fileMarkers.Length);
-        }
-
-        /// <summary>
-        /// Verify that the SQL parser correctly detects errors in text
-        /// </summary>
-        [Fact]
-        public void ParseSelectStatementWithoutErrors()
-        {
-            // sql statement with no errors
-            const string sqlWithErrors = "SELECT * FROM sys.objects";
-
-            // get the test service 
-            LanguageService service = TestObjects.GetTestLanguageService();
-
-            // parse the sql statement
-            var scriptFile = new ScriptFile();
-            scriptFile.SetFileContents(sqlWithErrors);
-            ScriptFileMarker[] fileMarkers = service.GetSemanticMarkers(scriptFile);
-
-            // verify there are no errors
-            Assert.Equal(0, fileMarkers.Length);
-        }
-
-        /// <summary>
-        /// Verify that the SQL parser correctly detects errors in text
-        /// </summary>
-        [Fact]
-        public void ParseSelectStatementWithError()
-        {
-            // sql statement with errors
-            const string sqlWithErrors = "SELECT *** FROM sys.objects";
-
-            // get test service
-            LanguageService service = TestObjects.GetTestLanguageService();
-
-            // parse sql statement
-            var scriptFile = new ScriptFile();
-            scriptFile.SetFileContents(sqlWithErrors);
-            ScriptFileMarker[] fileMarkers = service.GetSemanticMarkers(scriptFile);
-
-            // verify there is one error
-            Assert.Equal(1, fileMarkers.Length);
-
-            // verify the position of the error
-            Assert.Equal(9, fileMarkers[0].ScriptRegion.StartColumnNumber);
-            Assert.Equal(1, fileMarkers[0].ScriptRegion.StartLineNumber);
-            Assert.Equal(10, fileMarkers[0].ScriptRegion.EndColumnNumber);
-            Assert.Equal(1, fileMarkers[0].ScriptRegion.EndLineNumber);
-        }
-
-        /// <summary>
-        /// Verify that the SQL parser correctly detects errors in text
-        /// </summary>
-        [Fact]
-        public void ParseMultilineSqlWithErrors()
-        {
-            // multiline sql with errors
-            const string sqlWithErrors = 
-                "SELECT *** FROM sys.objects;\n" +
-                "GO\n" +
-                "SELECT *** FROM sys.objects;\n";
-
-            // get test service
-            LanguageService service = TestObjects.GetTestLanguageService();
-
-            // parse sql
-            var scriptFile = new ScriptFile();
-            scriptFile.SetFileContents(sqlWithErrors);
-            ScriptFileMarker[] fileMarkers = service.GetSemanticMarkers(scriptFile);
-
-            // verify there are two errors
-            Assert.Equal(2, fileMarkers.Length);
-
-            // check position of first error
-            Assert.Equal(9, fileMarkers[0].ScriptRegion.StartColumnNumber);
-            Assert.Equal(1, fileMarkers[0].ScriptRegion.StartLineNumber);
-            Assert.Equal(10, fileMarkers[0].ScriptRegion.EndColumnNumber);
-            Assert.Equal(1, fileMarkers[0].ScriptRegion.EndLineNumber);
-
-            // check position of second error
-            Assert.Equal(9, fileMarkers[1].ScriptRegion.StartColumnNumber);
-            Assert.Equal(3, fileMarkers[1].ScriptRegion.StartLineNumber);
-            Assert.Equal(10, fileMarkers[1].ScriptRegion.EndColumnNumber);
-            Assert.Equal(3, fileMarkers[1].ScriptRegion.EndLineNumber);
-        }
-
-        /// <summary>
-        /// Verify that GetSignatureHelp returns null when the provided TextDocumentPosition
-        /// has no associated ScriptParseInfo.
-        /// </summary>
-        [Fact]
-        public void GetSignatureHelpReturnsNullIfParseInfoNotInitialized()
-        {
-            // Given service doesn't have parseinfo intialized for a document
-            const string docContent = "SELECT * FROM sys.objects";
-            LanguageService service = TestObjects.GetTestLanguageService();
-            var scriptFile = new ScriptFile();
-            scriptFile.SetFileContents(docContent);
-
-            // When requesting SignatureHelp
-            SignatureHelp signatureHelp = service.GetSignatureHelp(TestObjects.GetTestDocPosition(), scriptFile);
-            
-            // Then null is returned as no parse info can be used to find the signature
-            Assert.Null(signatureHelp);
-        }
-
-        [Fact]
-        public void EmptyCompletionListTest()
-        {           
-            Assert.Equal(AutoCompleteHelper.EmptyCompletionList.Length, 0);
-        }
-
-        [Fact]
-        public void SetWorkspaceServiceInstanceTest()
-        {           
-            AutoCompleteHelper.WorkspaceServiceInstance = null;
-            // workspace will be recreated if it's set to null
-            Assert.NotNull(AutoCompleteHelper.WorkspaceServiceInstance);
-        }
-
-        internal class TestScriptDocumentInfo : ScriptDocumentInfo
-        {
-            public TestScriptDocumentInfo(TextDocumentPosition textDocumentPosition, ScriptFile scriptFile, ScriptParseInfo scriptParseInfo)
-                :base(textDocumentPosition, scriptFile, scriptParseInfo)
-            {
-                
-            }
-            
-            public override string TokenText
-            {
-                get
-                {
-                    return "doesntmatchanythingintheintellisensedefaultlist";
-                }
-            }
-        }
-
-        [Fact]
-        public void GetDefaultCompletionListWithNoMatchesTest()
-        {           
-            var scriptFile = new ScriptFile();
-            scriptFile.SetFileContents("koko wants a bananas");
-
-            ScriptParseInfo scriptInfo = new ScriptParseInfo { IsConnected = false };
-
-            var scriptDocumentInfo = new TestScriptDocumentInfo(
-                new TextDocumentPosition()
-                {
-                    TextDocument = new TextDocumentIdentifier() {  Uri = TestObjects.ScriptUri  },
-                    Position = new Position() { Line = 0, Character = 0 }
-                }, scriptFile, scriptInfo);
-      
-            AutoCompleteHelper.GetDefaultCompletionItems(scriptDocumentInfo, false);
-
-        }
-
-        #endregion
-
-        #region "General Language Service tests"
-
-
         private static void GetLiveAutoCompleteTestObjects(
             out TextDocumentPosition textDocument,
             out ScriptFile scriptFile,
@@ -211,7 +26,7 @@ namespace Microsoft.SqlTools.ServiceLayer.IntegrationTests.LanguageServer
         {
             textDocument = new TextDocumentPosition
             {
-                TextDocument = new TextDocumentIdentifier {Uri = TestObjects.ScriptUri},
+                TextDocument = new TextDocumentIdentifier { Uri = TestObjects.ScriptUri },
                 Position = new Position
                 {
                     Line = 0,
@@ -221,7 +36,7 @@ namespace Microsoft.SqlTools.ServiceLayer.IntegrationTests.LanguageServer
 
             connInfo = TestObjects.InitLiveConnectionInfo(out scriptFile);
         }
-        
+
         /// <summary>
         /// Test the service initialization code path and verify nothing throws
         /// </summary>
@@ -240,7 +55,7 @@ namespace Microsoft.SqlTools.ServiceLayer.IntegrationTests.LanguageServer
             Assert.True(LanguageService.ConnectionServiceInstance != null);
             Assert.True(LanguageService.Instance.CurrentSettings != null);
             Assert.True(LanguageService.Instance.CurrentWorkspace != null);
-        }  
+        }
 
         /// <summary>
         /// Test the service initialization code path and verify nothing throws
@@ -251,7 +66,7 @@ namespace Microsoft.SqlTools.ServiceLayer.IntegrationTests.LanguageServer
             ScriptFile scriptFile;
             ConnectionInfo connInfo = TestObjects.InitLiveConnectionInfo(out scriptFile);
 
-            ScriptParseInfo scriptInfo = new ScriptParseInfo {IsConnected = true};
+            ScriptParseInfo scriptInfo = new ScriptParseInfo { IsConnected = true };
 
             AutoCompleteHelper.PrepopulateCommonMetadata(connInfo, scriptInfo, null);
         }
@@ -272,7 +87,7 @@ namespace Microsoft.SqlTools.ServiceLayer.IntegrationTests.LanguageServer
 
             var autoCompleteService = LanguageService.Instance;
             var completions = autoCompleteService.GetCompletionItems(
-                textDocument, 
+                textDocument,
                 scriptFile,
                 connInfo);
 
@@ -289,6 +104,7 @@ namespace Microsoft.SqlTools.ServiceLayer.IntegrationTests.LanguageServer
         {
             // When we make a connection to a live database
             ScriptFile scriptFile;
+            Hosting.ServiceHost.SendEventIgnoreExceptions = true;
             ConnectionInfo connInfo = TestObjects.InitLiveConnectionInfo(out scriptFile);
 
             // And we place the cursor after a function that should prompt for signature help
@@ -307,16 +123,17 @@ namespace Microsoft.SqlTools.ServiceLayer.IntegrationTests.LanguageServer
                 }
             };
 
-            // If we have a valid ScriptParseInfo and the SQL has already been parsed
+            // If the SQL has already been parsed
             var service = LanguageService.Instance;
-            ScriptParseInfo parseInfo = service.GetScriptParseInfo(scriptFile.ClientFilePath);
             await service.UpdateLanguageServiceOnConnection(connInfo);
 
-            // We should get back a non-null SignatureHelp 
+            // We should get back a non-null ScriptParseInfo
+            ScriptParseInfo parseInfo = service.GetScriptParseInfo(scriptFile.ClientFilePath);
+            Assert.NotNull(parseInfo);
+
+            // And we should get back a non-null SignatureHelp
             SignatureHelp signatureHelp = service.GetSignatureHelp(textDocument, scriptFile);
             Assert.NotNull(signatureHelp);
         }
-
-        #endregion
     }
 }
