6b692608ff8e8803065f8ad483bf781742d2f479
Ben Russell <benrr101@outlook.com>
2017-01-20 13:44:04 -0800

    Refactoring of execute contracts into their own namespace

---
+++ src/Microsoft.SqlTools.ServiceLayer/QueryExecution/Contracts/ExecuteRequests/ExecuteDocumentSelectionRequest.cs
@@ -0,0 +1,27 @@
+﻿//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+using Microsoft.SqlTools.ServiceLayer.Hosting.Protocol.Contracts;
+
+namespace Microsoft.SqlTools.ServiceLayer.QueryExecution.Contracts.ExecuteRequests
+{
+    /// <summary>
+    /// Parameters for the query execute request
+    /// </summary>
+    public class ExecuteDocumentSelectionParams : ExecuteRequestParamsBase
+    {
+        /// <summary>
+        /// The selection from the document
+        /// </summary>
+        public SelectionData QuerySelection { get; set; }
+    }
+
+    public class QueryExecuteRequest
+    {
+        public static readonly
+            RequestType<ExecuteDocumentSelectionParams, ExecuteRequestResult> Type =
+            RequestType<ExecuteDocumentSelectionParams, ExecuteRequestResult>.Create("query/executeDocumentSelection");
+    }
+}
