95fdcd9bed9d205bade93bf5a1245e3fd30feb57
Ben Russell <benrr101@outlook.com>
2017-01-20 17:05:43 -0800

    Adding forgotten string request

---
+++ src/Microsoft.SqlTools.ServiceLayer/QueryExecution/Contracts/ExecuteRequests/ExecuteStringRequest.cs
@@ -0,0 +1,27 @@
+﻿//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+using Microsoft.SqlTools.ServiceLayer.Hosting.Protocol.Contracts;
+
+namespace Microsoft.SqlTools.ServiceLayer.QueryExecution.Contracts.ExecuteRequests
+{
+    /// <summary>
+    /// Parameters for executing a query directly
+    /// </summary>
+    public class ExecuteStringParams : ExecuteRequestParamsBase
+    {
+        /// <summary>
+        /// The query to execute
+        /// </summary>
+        public string Query { get; set; }
+    }
+
+    public class ExecuteStringRequest
+    {
+        public static readonly 
+            RequestType<ExecuteStringParams, ExecuteRequestResult> Type = 
+            RequestType<ExecuteStringParams, ExecuteRequestResult>.Create("query/executeString");
+    }
+}
