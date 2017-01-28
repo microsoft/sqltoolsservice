d6f937c05f091522cbd059051e69b5b2bdfb7a21
Aditya Bist <adbist@microsoft.com>
2017-01-27 17:26:18 -0800

    changed baseline files

--- test/Microsoft.SqlTools.ServiceLayer.Test/BatchParser/BatchParserTests.cs
+++ test/Microsoft.SqlTools.ServiceLayer.Test/BatchParser/BatchParserTests.cs
@@ -232,7 +232,6 @@ namespace Microsoft.SqlTools.ServiceLayer.Test.BatchParser
             }
 
             string outputString = output.ToString();
-            Console.WriteLine(outputString);
 
             if (string.Compare(baseline, outputString, StringComparison.Ordinal) != 0)
             {
