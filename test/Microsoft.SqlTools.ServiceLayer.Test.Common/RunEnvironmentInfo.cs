5e743fd5187b849e80543c455a603986cddd60b0
Kevin Cunnane <kcunnane@microsoft.com>
2017-02-10 12:29:18 -0800

    Fix test file path lookup on Mac

--- test/Microsoft.SqlTools.ServiceLayer.Test.Common/RunEnvironmentInfo.cs
+++ test/Microsoft.SqlTools.ServiceLayer.Test.Common/RunEnvironmentInfo.cs
@@ -5,7 +5,9 @@
 
 using System;
 using System.IO;
+using System.Linq;
 using System.Reflection;
+using System.Runtime.InteropServices;
 
 namespace Microsoft.SqlTools.ServiceLayer.Test.Common
 {
@@ -33,7 +35,7 @@ namespace Microsoft.SqlTools.ServiceLayer.Test.Common
         public static string GetTestDataLocation()
         {
             string testFolderPath;
-            string testPath = @"test\Microsoft.SqlTools.ServiceLayer.Test.Common";
+            string testPath = Path.Combine("test", "Microsoft.SqlTools.ServiceLayer.Test.Common", "TestData");
             string projectPath = Environment.GetEnvironmentVariable(Constants.ProjectPath);
 
             if (projectPath != null)
@@ -51,13 +53,21 @@ namespace Microsoft.SqlTools.ServiceLayer.Test.Common
                     // We are running tests locally, which means we expect to be running inside the bin\debug\netcoreapp directory
                     // Test Files should be found at the root of the project so go back the necessary number of directories for this 
                     // to be found. We are manually specifying the testFolderPath here for clarity on where to expect this
-                    string defaultPath = Path.Combine(typeof(Scripts).GetTypeInfo().Assembly.Location, @"..\..\..\..\..");
-                    testFolderPath = Path.Combine(defaultPath, @"Microsoft.SqlTools.ServiceLayer.Test.Common");
+
+                    string assemblyDir = Path.GetDirectoryName(typeof(Scripts).GetTypeInfo().Assembly.Location);
+                    string defaultPath = Path.Combine(assemblyDir, GoUpNDirectories(4));
+                    testFolderPath = Path.Combine(defaultPath, "Microsoft.SqlTools.ServiceLayer.Test.Common", "TestData");
+                    
                     cachedTestFolderPath = testFolderPath;
                 }
             }
             return testFolderPath;
         }
 
+        private static string GoUpNDirectories(int n)
+        {
+            string up = ".." + (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "\\" : "/");
+            return string.Concat(Enumerable.Repeat(up, n));
+        }
     }
 }
