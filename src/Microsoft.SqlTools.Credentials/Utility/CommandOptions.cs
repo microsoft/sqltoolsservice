b8e32f8362c9743aa12c241f8639ce56835835c9
Karl Burtram <karlb@microsoft.com>
2017-02-21 14:52:02 -0800

    Initial credential service files

--- src/Microsoft.SqlTools.Credentials/Utility/CommandOptions.cs
+++ src/Microsoft.SqlTools.Credentials/Utility/CommandOptions.cs
@@ -4,9 +4,8 @@
 //
 
 using System;
-using System.Globalization;
 
-namespace Microsoft.SqlTools.ServiceLayer.Utility
+namespace Microsoft.SqlTools.Credentials.Utility
 {
     /// <summary>
     /// The command-line options helper class.
@@ -19,7 +18,6 @@ namespace Microsoft.SqlTools.ServiceLayer.Utility
         public CommandOptions(string[] args)
         {
             ErrorMessage = string.Empty;
-            Locale = string.Empty;
 
             try
             {
@@ -28,31 +26,17 @@ namespace Microsoft.SqlTools.ServiceLayer.Utility
                     string arg = args[i];
                     if (arg.StartsWith("--") || arg.StartsWith("-"))
                     {
-                        // Extracting arguments and properties
-                        arg = arg.Substring(1).ToLowerInvariant();
-                        string argName = arg;
-                        string argProperty = "";
-                        int splitIndex = arg.IndexOf(' ');
-                        if (splitIndex > 0)
-                        {
-                            argName = arg.Substring(0, splitIndex);
-                            argProperty = arg.Substring(splitIndex + 1);
-                        }
-
-                        switch (argName)
+                        switch (arg)
                         {
                             case "-enable-logging":
                                 EnableLogging = true;
-                                break;
-                            case "-locale":
-                                SetLocale(argProperty);
                                 break;
                             case "h":
                             case "-help":
                                 ShouldExit = true;
                                 return;
                             default:
-                                ErrorMessage += String.Format("Unknown argument \"{0}\" with property \"{1}\"" + Environment.NewLine, argName, argProperty);
+                                ErrorMessage += String.Format("Unknown argument \"{0}\"" + Environment.NewLine, arg);
                                 break;
                         }
                     }
@@ -75,7 +59,6 @@ namespace Microsoft.SqlTools.ServiceLayer.Utility
 
         internal string ErrorMessage { get; private set; }
 
-
         /// <summary>
         /// Whether diagnostic logging is enabled
         /// </summary>
@@ -87,11 +70,6 @@ namespace Microsoft.SqlTools.ServiceLayer.Utility
         public bool ShouldExit { get; private set; }
 
         /// <summary>
-        /// The locale our we should instantiate this service in 
-        /// </summary>
-        public string Locale { get; private set; }
-
-        /// <summary>
         /// Get the usage string describing command-line arguments for the program
         /// </summary>
         public string Usage
@@ -99,34 +77,12 @@ namespace Microsoft.SqlTools.ServiceLayer.Utility
             get
             {
                 var str = string.Format("{0}" + Environment.NewLine +
-                    "Microsoft.SqlTools.ServiceLayer.exe " + Environment.NewLine +
+                    "Microsoft.SqlTools.Credentials " + Environment.NewLine +
                     "   Options:" + Environment.NewLine +
                     "        [--enable-logging]" + Environment.NewLine +
-                    "        [--help]" + Environment.NewLine +
-                    "        [--locale **] (default: 'en')" + Environment.NewLine,
+                    "        [--help]" + Environment.NewLine,
                     ErrorMessage);
                 return str;
-            }
-        }
-
-        private void SetLocale(string locale){
-            try
-            {
-                // Creating cultureInfo from our given locale
-                CultureInfo language = new CultureInfo(locale);
-                Locale = locale;
-
-                // Setting our language globally 
-                CultureInfo.CurrentCulture = language;
-                CultureInfo.CurrentUICulture = language;
-
-                // Setting our internal SR culture to our global culture
-                SR.Culture = CultureInfo.CurrentCulture;
-            }
-            catch(Exception ex)
-            {
-                // Warn user of invalid locale, and fall back to english 
-                Console.WriteLine(ex);
             }
         }
     }
