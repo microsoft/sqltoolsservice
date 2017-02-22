dbeb45eb1cb8e08b9c2606cddfb2895dc01d225f
Ben Russell <benrr101@outlook.com>
2017-02-22 14:24:24 -0800

    Properly wiring up the settings for query execution service

--- test/Microsoft.SqlTools.ServiceLayer.Test/QueryExecution/SettingsTests.cs
+++ test/Microsoft.SqlTools.ServiceLayer.Test/QueryExecution/SettingsTests.cs
@@ -3,6 +3,9 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 //
 
+using Microsoft.SqlTools.ServiceLayer.Hosting.Protocol;
+using Microsoft.SqlTools.ServiceLayer.QueryExecution;
+using Microsoft.SqlTools.ServiceLayer.QueryExecution.Contracts;
 using Microsoft.SqlTools.ServiceLayer.SqlContext;
 using Newtonsoft.Json.Linq;
 using Xunit;
@@ -19,7 +22,7 @@ namespace Microsoft.SqlTools.ServiceLayer.Test.QueryExecution
 
             // Then: The default values should be as expected
             Assert.Equal("GO", sqlToolsSettings.QueryExecutionSettings.BatchSeparator);
-            Assert.Equal(64*1024, sqlToolsSettings.QueryExecutionSettings.MaxCharsToStore);
+            Assert.Equal(ushort.MaxValue, sqlToolsSettings.QueryExecutionSettings.MaxCharsToStore);
             Assert.Equal(2*1024*1024, sqlToolsSettings.QueryExecutionSettings.MaxXmlCharsToStore);
             Assert.False(sqlToolsSettings.QueryExecutionSettings.ExecutionPlanOptions.IncludeActualExecutionPlanXml);
             Assert.False(sqlToolsSettings.QueryExecutionSettings.ExecutionPlanOptions.IncludeEstimatedExecutionPlanXml);
@@ -48,6 +51,41 @@ namespace Microsoft.SqlTools.ServiceLayer.Test.QueryExecution
 
             // Then: The values defined in the JSON should propagate to the setting object
             Assert.False(sqlToolsSettings.QueryExecutionSettings.DisplayBitAsNumber);
+        }
+
+        [Fact]
+        public void ValidateSettingsObjectUpdates()
+        {
+            // If: I update a settings object with a new settings object
+            var qes = new QueryExecutionService(null, null);
+            SqlToolsSettings settings = new SqlToolsSettings()
+            {
+                SqlTools = new SqlToolsSettingsValues
+                {
+                    QueryExecutionSettings = new QueryExecutionSettings
+                    {
+                        DisplayBitAsNumber = false,
+                        MaxXmlCharsToStore = 1,
+                        MaxCharsToStore = 1,
+                        ExecutionPlanOptions = new ExecutionPlanOptions
+                        {
+                            IncludeActualExecutionPlanXml = true,
+                            IncludeEstimatedExecutionPlanXml = true
+                        },
+                        BatchSeparator = "YO"
+                    }
+                }
+                    
+            };
+            qes.UpdateSettings(null, settings, new EventContext());
+
+            // Then: The settings object should match what it was updated to
+            Assert.False(qes.Settings.QueryExecutionSettings.DisplayBitAsNumber);
+            Assert.True(qes.Settings.QueryExecutionSettings.ExecutionPlanOptions.IncludeActualExecutionPlanXml);
+            Assert.True(qes.Settings.QueryExecutionSettings.ExecutionPlanOptions.IncludeEstimatedExecutionPlanXml);
+            Assert.Equal(1, qes.Settings.QueryExecutionSettings.MaxCharsToStore);
+            Assert.Equal(1, qes.Settings.QueryExecutionSettings.MaxXmlCharsToStore);
+            Assert.Equal("YO", qes.Settings.QueryExecutionSettings.BatchSeparator);
         }
 
     }
