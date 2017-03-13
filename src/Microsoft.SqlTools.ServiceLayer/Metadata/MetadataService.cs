f7481f0b5693003ab2039e50feb201e6ce91c3a0
kburtram <karlb@microsoft.com>
2017-03-13 13:31:10 -0700

    Remove SMO from metadata service.

--- src/Microsoft.SqlTools.ServiceLayer/Metadata/MetadataService.cs
+++ src/Microsoft.SqlTools.ServiceLayer/Metadata/MetadataService.cs
@@ -5,11 +5,8 @@
 
 using System;
 using System.Collections.Generic;
-using System.Data.Common;
 using System.Data.SqlClient;
 using System.Threading.Tasks;
-using Microsoft.SqlServer.Management.Common;
-using Microsoft.SqlServer.Management.Smo;
 using Microsoft.SqlTools.Hosting.Protocol;
 using Microsoft.SqlTools.ServiceLayer.Connection;
 using Microsoft.SqlTools.ServiceLayer.Hosting;
@@ -84,6 +81,48 @@ namespace Microsoft.SqlTools.ServiceLayer.Metadata
             return null;
         }
 
+        internal static void ReadMetadata(SqlConnection sqlConn, List<ObjectMetadata> metadata)
+        {
+            string sql = 
+                @"SELECT object_id, s.name AS schema_name, o.[name] AS object_name, 
+                    o.[type] AS object_type, o.[type_desc] AS object_type_desc, is_ms_shipped
+                    FROM sys.all_objects o
+                    INNER JOIN sys.schemas s ON o.schema_id = s.schema_id
+                    WHERE o.is_ms_shipped != 1
+                    AND (o.[type] = 'P' OR o.[type] = 'V' OR o.[type] = 'U')
+                    ORDER BY object_type_desc, schema_name, object_name";
+
+            SqlCommand sqlCommand = new SqlCommand(sql, sqlConn);
+            var reader = sqlCommand.ExecuteReader();
+            while (reader.Read())
+            {
+                var schemaName = reader[1] as string;
+                var objectName = reader[2] as string;
+                var objectType = reader[3] as string;
+
+                MetadataType metadataType;
+                if (objectType.StartsWith("V"))
+                {
+                    metadataType = MetadataType.View;
+                }
+                else if (objectType.StartsWith("P"))
+                {
+                    metadataType = MetadataType.SProc;
+                }
+                else
+                {
+                    metadataType = MetadataType.Table;
+                }
+
+                metadata.Add(new ObjectMetadata
+                {
+                    MetadataType = metadataType,
+                    Schema = schemaName,
+                    ObjectName = objectName
+                });
+            }
+        }
+
         internal static async Task HandleMetadataListRequest(
             MetadataQueryParams metadataParams,
             RequestContext<MetadataQueryResult> requestContext)
@@ -101,38 +140,7 @@ namespace Microsoft.SqlTools.ServiceLayer.Metadata
                 {
                     SqlConnection sqlConn = OpenMetadataConnection(connInfo);
 
-                    // populate the binding context to work with the SMO metadata provider
-                    ServerConnection serverConn = new ServerConnection(sqlConn);
-                     // Reuse existing connection
-                    Server server = new Server(serverConn);
-                    // The default database name is the database name of the server connection
-                    string dbName = serverConn.DatabaseName;
-                    if (connInfo != null)
-                    {
-                        // If there is a query DbConnection, use that connection to get the database name
-                        // This is preferred since it has the most current database name (in case of database switching)
-                        DbConnection connection;
-                        if (connInfo.TryGetConnection(Connection.ConnectionType.Query, out connection))
-                        {
-                            if (!string.IsNullOrEmpty(connection.Database))
-                            {
-                                dbName  = connection.Database;
-                            }
-                        }
-                    }
-                    
-                    var database = new Database(server, dbName);
-                    database.Refresh();
-
-                    foreach (Table table in database.Tables)
-                    {
-                        metadata.Add(new ObjectMetadata
-                        {
-                            MetadataType = MetadataType.Table,
-                            Schema = table.Schema,
-                            ObjectName = table.Name
-                        });
-                    }                      
+                    ReadMetadata(sqlConn, metadata);                   
                 }
                 catch (Exception)
                 {
