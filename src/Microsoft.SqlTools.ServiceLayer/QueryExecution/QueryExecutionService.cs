86929795f21ba271b06b158971032308c5ceb89b
Ben Russell <benrr101@outlook.com>
2017-03-13 16:57:50 -0700

    Creating an interservice API for getting query result subsets

--- src/Microsoft.SqlTools.ServiceLayer/QueryExecution/QueryExecutionService.cs
+++ src/Microsoft.SqlTools.ServiceLayer/QueryExecution/QueryExecutionService.cs
@@ -169,19 +169,10 @@ namespace Microsoft.SqlTools.ServiceLayer.QueryExecution
         {
             try
             {
-                // Attempt to load the query
-                Query query;
-                if (!ActiveQueries.TryGetValue(subsetParams.OwnerUri, out query))
-                {
-                    await requestContext.SendError(SR.QueryServiceRequestsNoQuery);
-                    return;
-                }
-
-                // Retrieve the requested subset and return it
+                ResultSetSubset subset = await InterServiceResultSubset(subsetParams);
                 var result = new SubsetResult
                 {
-                    ResultSubset = await query.GetSubset(subsetParams.BatchIndex,
-                        subsetParams.ResultSetIndex, subsetParams.RowsStartIndex, subsetParams.RowsCount)
+                    ResultSubset = subset
                 };
                 await requestContext.SendResult(result);
             }
@@ -373,7 +364,6 @@ namespace Microsoft.SqlTools.ServiceLayer.QueryExecution
         /// <param name="ownerUri">The identifier of the query to be disposed</param>
         /// <param name="successAction">Action to perform on success</param>
         /// <param name="failureAction">Action to perform on failure</param>
-        /// <returns></returns>
         public async Task InterServiceDisposeQuery(string ownerUri, Func<Task> successAction,
             Func<string, Task> failureAction)
         {
@@ -400,6 +390,28 @@ namespace Microsoft.SqlTools.ServiceLayer.QueryExecution
             {
                 await failureAction(e.Message);
             }
+        }
+
+        /// <summary>
+        /// Retrieves the requested subset of rows from the requested result set. Intended to
+        /// </summary>
+        /// <param name="subsetParams">Parameters for the subset to retrieve</param>
+        /// <returns>The requested subset</returns>
+        /// <exception cref="ArgumentOutOfRangeException">The requested query does not exist</exception>
+        public async Task<ResultSetSubset> InterServiceResultSubset(SubsetParams subsetParams)
+        {
+            Validate.IsNotNullOrEmptyString(nameof(subsetParams.OwnerUri), subsetParams.OwnerUri);
+
+            // Attempt to load the query
+            Query query;
+            if (!ActiveQueries.TryGetValue(subsetParams.OwnerUri, out query))
+            {
+                throw new ArgumentOutOfRangeException(SR.QueryServiceRequestsNoQuery);
+            }
+
+            // Retrieve the requested subset and return it
+            return await query.GetSubset(subsetParams.BatchIndex, subsetParams.ResultSetIndex,
+                subsetParams.RowsStartIndex, subsetParams.RowsCount);
         }
 
         #endregion
