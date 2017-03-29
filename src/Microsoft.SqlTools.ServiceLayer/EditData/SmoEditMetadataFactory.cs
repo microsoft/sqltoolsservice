fc04f6822f59b11b89e640c76c380e249784182f
Raymond Martin <Raymondd@users.noreply.github.com>
2017-03-28 18:35:41 -0700

    Fix edit data isMemoryOptimized issue  (#298)
    * adding refresh
    
    * adding a comment
    
    * spelling

--- src/Microsoft.SqlTools.ServiceLayer/EditData/SmoEditMetadataFactory.cs
+++ src/Microsoft.SqlTools.ServiceLayer/EditData/SmoEditMetadataFactory.cs
@@ -75,6 +75,9 @@ namespace Microsoft.SqlTools.ServiceLayer.EditData
                     throw new ArgumentOutOfRangeException(nameof(objectType), SR.EditDataUnsupportedObjectType(objectType));
             }
 
+            // A bug in SMO makes it necessary to call refresh to attain certain properties (such as IsMemoryOptimized)
+            smoResult.Refresh();
+            
             // Generate the edit column metadata
             List<EditColumnMetadata> editColumns = new List<EditColumnMetadata>();
             for (int i = 0; i < smoResult.Columns.Count; i++)
