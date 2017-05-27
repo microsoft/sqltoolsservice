0d00534f6aebf9b706a89d928b63ae8e122677a4
Leila Lali <llali@microsoft.com>
2017-05-26 21:54:45 -0700

    setting timeout for oe tasks (#363)

--- src/Microsoft.SqlTools.ServiceLayer/ObjectExplorer/SmoModel/SqlHistoryTableQuerier.cs
+++ src/Microsoft.SqlTools.ServiceLayer/ObjectExplorer/SmoModel/SqlHistoryTableQuerier.cs
@@ -3,6 +3,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 //
 
+using System;
 using Microsoft.SqlServer.Management.Smo;
 
 namespace Microsoft.SqlTools.ServiceLayer.ObjectExplorer.SmoModel
@@ -16,8 +17,19 @@ namespace Microsoft.SqlTools.ServiceLayer.ObjectExplorer.SmoModel
         {
             Table parentTable = parent as Table;
             Table historyTable = smoObject as Table;
-
-            return (parentTable.HistoryTableID == historyTable.ID);
+            if (parentTable != null && historyTable != null)
+            {
+                try
+                {
+                    return (parentTable.HistoryTableID == historyTable.ID);
+                }
+                catch(Exception)
+                {
+                    //TODO: have a better filtering here. HistoryTable is not available for SQL 2014.
+                    //and the property throws exception here
+                }
+            }
+            return false;
         }
     }
 }
