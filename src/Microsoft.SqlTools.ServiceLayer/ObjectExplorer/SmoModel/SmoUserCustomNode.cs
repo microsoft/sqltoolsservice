f50796ed199dd27a7e1fd6c14a747c017e085968
Leila Lali <llali@microsoft.com>
2017-06-12 16:36:59 -0700

    setting user status in oe if disabled

---
+++ src/Microsoft.SqlTools.ServiceLayer/ObjectExplorer/SmoModel/SmoUserCustomNode.cs
@@ -0,0 +1,38 @@
+﻿//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+using Microsoft.SqlServer.Management.Smo;
+
+namespace Microsoft.SqlTools.ServiceLayer.ObjectExplorer.SmoModel
+{
+    /// <summary>
+    /// Status for logins
+    /// </summary>
+    internal partial class UsersChildFactory : SmoChildFactoryBase
+    {
+        public override string GetNodeStatus(object context)
+        {
+            return UserCustomeNodeHelper.GetStatus(context);
+        }
+    }
+
+    internal static class UserCustomeNodeHelper
+    {
+        internal static string GetStatus(object context)
+        {
+            User user = context as User;
+            if (user != null)
+            {
+                if (!user.HasDBAccess)
+                {
+                    return "Disabled";
+                }
+            }
+
+            return string.Empty;
+        }
+    }
+}
+
