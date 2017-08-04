30799143df493bd8017c86669cd9dcc917cabbb7
Anthony Dresser <andresse@microsoft.com>
2017-08-04 12:14:27 -0700

    Changed how the test awaits on executed queries to make it more reliable

--- src/Microsoft.SqlTools.ServiceLayer/QueryExecution/ResultOnlyContext.cs
+++ src/Microsoft.SqlTools.ServiceLayer/QueryExecution/ResultOnlyContext.cs
@@ -15,10 +15,10 @@ namespace Microsoft.SqlTools.ServiceLayer.QueryExecution
     /// </summary>
     public class ResultOnlyContext<TResult> : IEventSender
     {
-        private readonly RequestContext<TResult> origContext;
+        private readonly RequestContext<TResult> OrigContext;
 
         public ResultOnlyContext(RequestContext<TResult> context) {
-            origContext = context;
+            OrigContext = context;
         }
 
         public virtual Task SendEvent<TParams>(EventType<TParams> eventType, TParams eventParams)
@@ -26,6 +26,11 @@ namespace Microsoft.SqlTools.ServiceLayer.QueryExecution
             // no op to swallow events
             // in the future this could be used to roll up events and send them back in the result
             return Task.FromResult(true);
+        }
+
+        public virtual Task SendError(string errorMessage, int errorCode = 0)
+        {
+            return OrigContext.SendError(errorMessage, errorCode);
         }
     }
 }
