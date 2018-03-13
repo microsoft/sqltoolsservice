674f83e444285472978e589d0318db18ca2fb6fb
Karl Burtram <karlb@microsoft.com>
2018-03-13 15:47:12 -0700

    Try agent directory

--- scripts/CleanupKubernetesTarget.ps1
+++ scripts/CleanupKubernetesTarget.ps1
@@ -3,7 +3,7 @@
 )
 $env:KUBECONFIG="kubeconfig.centralus.json"
 
-$kube = $env:SYSTEM_DEFAULTWORKINGDIRECTORY + "\kubectl.exe"
+$kube = $env:AGENT_WORKFOLDER + "\kubectl.exe"
 
 iex "$kube delete service $svcName"
 iex "$kube delete deployment $svcName"
