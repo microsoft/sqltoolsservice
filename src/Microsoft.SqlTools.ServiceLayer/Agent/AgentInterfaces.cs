897ae2d3e29a97c60be17a3a41c6f60d00c83f30
Karl Burtram <karlb@microsoft.com>
2018-05-01 13:54:24 -0700

    Change to trigger CI build

--- src/Microsoft.SqlTools.ServiceLayer/Agent/AgentInterfaces.cs
+++ src/Microsoft.SqlTools.ServiceLayer/Agent/AgentInterfaces.cs
@@ -3,6 +3,8 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 //
 
+// Change something to trigger a CI build
+
 namespace Microsoft.SqlTools.ServiceLayer.Agent
 {
     interface IFilterDefinition
