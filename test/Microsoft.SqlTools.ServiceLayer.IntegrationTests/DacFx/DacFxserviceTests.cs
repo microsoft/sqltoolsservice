51a54c278c19f99a10b827ba760acfdc4157a13c
Kim Santiago <kisantia@microsoft.com>
2018-10-23 15:04:51 -0700

    Removed hardcoded values from dacfx export test

--- test/Microsoft.SqlTools.ServiceLayer.IntegrationTests/DacFx/DacFxserviceTests.cs
+++ test/Microsoft.SqlTools.ServiceLayer.IntegrationTests/DacFx/DacFxserviceTests.cs
@@ -1,8 +1,9 @@
 ﻿using Microsoft.SqlTools.Hosting.Protocol;
 using Microsoft.SqlTools.ServiceLayer.DacFx;
 using Microsoft.SqlTools.ServiceLayer.DacFx.Contracts;
+using Microsoft.SqlTools.ServiceLayer.IntegrationTests.Utility;
+using Microsoft.SqlTools.ServiceLayer.Test.Common;
 using Moq;
-using System.Data.SqlClient;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -10,23 +11,30 @@ namespace Microsoft.SqlTools.ServiceLayer.IntegrationTests.DacFx
 {
     public class DacFxServiceTests
     {
+
+        private LiveConnectionHelper.TestConnectionResult GetLiveAutoCompleteTestObjects()
+        {
+            var result = LiveConnectionHelper.InitLiveConnectionInfo();
+            return result;
+        }
+
         private async Task<Mock<RequestContext<DacFxExportResult>>> SendAndValidateDacFxExportRequest()
         {
+            var result = GetLiveAutoCompleteTestObjects();
             var requestContext = new Mock<RequestContext<DacFxExportResult>>();
             requestContext.Setup(x => x.SendResult(It.IsAny<DacFxExportResult>())).Returns(Task.FromResult(new object()));
-            var builder = new SqlConnectionStringBuilder();
-            builder.DataSource = "KISANTIA-HQ";
-            builder.InitialCatalog = "customerrepro";
-            builder.UserID = "test";
-            builder.Password = "Yukon901";
+
+            SqlTestDb testdb = await SqlTestDb.CreateNewAsync(TestServerType.OnPrem, false, "dacfxexporttestdb");
 
             var scriptingParams = new DacFxExportParams
             {
-                ConnectionString = builder.ConnectionString
+                ConnectionString = testdb.ConnectionString
             };
 
             DacFxService service = new DacFxService();
             await service.HandleExportRequest(scriptingParams, requestContext.Object);
+
+            testdb.Cleanup();
 
             return requestContext;
         }
