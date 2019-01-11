11032ef6dfac931203a8e62e599e95d1d7df3d81
Niranjan Dharmarajan <v-nidhar@microsoft.com>
2019-01-10 19:45:48 -0800

    Removing duplicate String files from ServiceLayer

---
+++ Microsoft.SqlTools.ManagedBatchParser.UnitTests/SrTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using ServiceLayerSr = Microsoft.SqlTools.ManagedBatchParser.SR;
+namespace Microsoft.SqlTools.ManagedBatchParser
+{
+    class SrTests
+    {
+    }
+}
---
+++ src/Microsoft.SqlTools.ManagedBatchParser/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Microsoft.SqlTools.ManagedBatchParser.Properties")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Microsoft.SqlTools.ManagedBatchParser.Properties")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("82dd9738-2ad3-4eb3-9f80-18b594e03621")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: InternalsVisibleTo("Microsoft.SqlTools.ServiceLayer.UnitTests")]
+[assembly: InternalsVisibleTo("Microsoft.SqlTools.ServiceLayer.IntegrationTests")]
+[assembly: InternalsVisibleTo("Microsoft.SqlTools.ServiceLayer.Test.Common")]
+[assembly: InternalsVisibleTo("MicrosoftSqlToolsServiceLayer")]
+[assembly: InternalsVisibleTo("Microsoft.SqlTools.ManagedBatchParser.LocalizationTests")]
