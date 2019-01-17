096f67ab32641a96b6ce323fa4788f500522e7ff
Niranjan Dharmarajan <v-nidhar@microsoft.com>
2019-01-17 06:07:14 -0800

    New project "DummyProject_13" is added in old  version MsBuild

---
+++ test/DummyProject_13/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DummyProject_13
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
