0daad251e1b6bcf831bc61e0c36bbe47b0573c26
Kim Santiago <kisantia@microsoft.com>
2019-01-29 17:56:54 -0800

    simplyfing deploy options

--- src/Microsoft.SqlTools.ServiceLayer/DacFx/GenerateDeployPlanOperation.cs
+++ src/Microsoft.SqlTools.ServiceLayer/DacFx/GenerateDeployPlanOperation.cs
@@ -31,89 +31,23 @@ namespace Microsoft.SqlTools.ServiceLayer.DacFx
         public override void Execute()
         {
             DacPackage dacpac = DacPackage.Load(this.Parameters.PackageFilePath);
-            DacDeployOptions options = GetDefaultDeployOptions();
-            // this is so that operations with possible data loss will be in the report
-            options.BlockOnPossibleDataLoss = false;
-
+            DacDeployOptions options = GetDefaultDeployPlanOptions();
             DeployReport = this.DacServices.GenerateDeployReport(dacpac, this.Parameters.DatabaseName, options, this.CancellationToken);
         }
 
-        private DacDeployOptions GetDefaultDeployOptions()
+        private DacDeployOptions GetDefaultDeployPlanOptions()
         {
             DacDeployOptions options = new DacDeployOptions
             {
                 AllowDropBlockingAssemblies = true,
                 AllowIncompatiblePlatform = true,
-                BackupDatabaseBeforeChanges = false,
-                BlockOnPossibleDataLoss = true,
-                BlockWhenDriftDetected = true,
-                CompareUsingTargetCollation = false,
-                CommentOutSetVarDeclarations = false,
-                CreateNewDatabase = false,
-                DeployDatabaseInSingleUserMode = false,
-                DisableAndReenableDdlTriggers = true,
-                DoNotAlterReplicatedObjects = true,
-                DoNotAlterChangeDataCaptureObjects = true,
-                DropConstraintsNotInSource = true,
-                DropDmlTriggersNotInSource = true,
-                DropExtendedPropertiesNotInSource = true,
-                DropIndexesNotInSource = true,
+                BlockOnPossibleDataLoss = false,
                 DropObjectsNotInSource = true,
                 DropPermissionsNotInSource = true,
                 DropRoleMembersNotInSource = true,
-                GenerateSmartDefaults = false,
-                IgnoreAnsiNulls = false,
-                IgnoreAuthorizer = false,
-                IgnoreColumnCollation = false,
-                IgnoreComments = false,
-                IgnoreCryptographicProviderFilePath = true,
-                IgnoreDdlTriggerOrder = false,
-                IgnoreDdlTriggerState = false,
-                IgnoreDefaultSchema = false,
-                IgnoreDmlTriggerOrder = false,
-                IgnoreDmlTriggerState = false,
-                IgnoreExtendedProperties = false,
-                IgnoreFileAndLogFilePath = true,
-                IgnoreFilegroupPlacement = true,
-                IgnoreFileSize = true,
-                IgnoreFillFactor = true,
-                IgnoreFullTextCatalogFilePath = true,
-                IgnoreIdentitySeed = false,
-                IgnoreIncrement = false,
-                IgnoreIndexOptions = false,
-                IgnoreIndexPadding = true,
                 IgnoreKeywordCasing = false,
-                IgnoreLockHintsOnIndexes = false,
-                IgnoreLoginSids = true,
-                IgnoreNotForReplication = false,
-                IgnoreObjectPlacementOnPartitionScheme = true,
-                IgnorePartitionSchemes = false,
-                IgnorePermissions = false,
-                IgnoreQuotedIdentifiers = false,
-                IgnoreRoleMembership = false,
-                IgnoreRouteLifetime = true,
                 IgnoreSemicolonBetweenStatements = false,
-                IgnoreTableOptions = false,
-                IgnoreUserSettingsObjects = false,
                 IgnoreWhitespace = false,
-                IgnoreWithNocheckOnCheckConstraints = false,
-                IgnoreWithNocheckOnForeignKeys = false,
-                IncludeCompositeObjects = false,
-                IncludeTransactionalScripts = false,
-                NoAlterStatementsToChangeClrTypes = false,
-                PopulateFilesOnFileGroups = true,
-                RegisterDataTierApplication = false,
-                ScriptDatabaseCollation = false,
-                ScriptDatabaseCompatibility = false,
-                ScriptDatabaseOptions = true,
-                ScriptDeployStateChecks = false,
-                ScriptFileSize = false,
-                ScriptNewConstraintValidation = true,
-                ScriptRefreshModule = true,
-                TreatVerificationErrorsAsWarnings = false,
-                UnmodifiableObjectWarnings = true,
-                VerifyDeployment = true,
-                VerifyCollationCompatibility = true
             };
 
             return options;
