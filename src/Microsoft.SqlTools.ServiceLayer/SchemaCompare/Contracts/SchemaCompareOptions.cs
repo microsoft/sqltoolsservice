9cd3189d6747168e4a315e341cace239cb541170
Udeesha Gautam <udgautam@microsoft.com>
2019-04-16 11:28:47 -0700

    Removing the unnecessary default value attribute

--- src/Microsoft.SqlTools.ServiceLayer/SchemaCompare/Contracts/SchemaCompareOptions.cs
+++ src/Microsoft.SqlTools.ServiceLayer/SchemaCompare/Contracts/SchemaCompareOptions.cs
@@ -13,42 +13,35 @@ namespace Microsoft.SqlTools.ServiceLayer.SchemaCompare.Contracts
 {
     /// <summary>
     /// Class to define schema compare and publish options. 
-    /// Keeping the same defaults as DacFx
+    /// Keeping the order and defaults same as DacFx
     /// The default values here should also match the default values in ADS UX
     /// </summary>
     public class SchemaCompareOptions
     {
         public bool IgnoreTableOptions { get; set; }
 
-        [DefaultValue(true)]
         public bool IgnoreSemicolonBetweenStatements { get; set; } = true;
 
-        [DefaultValue(true)]
         public bool IgnoreRouteLifetime { get; set; } = true;
 
-        public bool IgnoreRoleMembership { get; set; } 
+        public bool IgnoreRoleMembership { get; set; }
 
-        [DefaultValue(true)]
         public bool IgnoreQuotedIdentifiers { get; set; } = true;
 
         public bool IgnorePermissions { get; set; }
 
         public bool IgnorePartitionSchemes { get; set; }
 
-        [DefaultValue(true)]
         public bool IgnoreObjectPlacementOnPartitionScheme { get; set; } = true;
 
         public bool IgnoreNotForReplication { get; set; }
 
-        [DefaultValue(true)]
         public bool IgnoreLoginSids { get; set; } = true;
 
         public bool IgnoreLockHintsOnIndexes { get; set; }
 
-        [DefaultValue(true)]
         public bool IgnoreKeywordCasing { get; set; } = true;
-
-        [DefaultValue(true)]
+        
         public bool IgnoreIndexPadding { get; set; } = true;
 
         public bool IgnoreIndexOptions { get; set; }
@@ -58,27 +51,21 @@ namespace Microsoft.SqlTools.ServiceLayer.SchemaCompare.Contracts
         public bool IgnoreIdentitySeed { get; set; }
 
         public bool IgnoreUserSettingsObjects { get; set; }
-
-        [DefaultValue(true)]
+        
         public bool IgnoreFullTextCatalogFilePath { get; set; } = true;
-
-        [DefaultValue(true)]
+        
         public bool IgnoreWhitespace { get; set; } = true;
 
         public bool IgnoreWithNocheckOnForeignKeys { get; set; }
-
-        [DefaultValue(true)]
+        
         public bool VerifyCollationCompatibility { get; set; } = true;
-
-        [DefaultValue(true)]
+        
         public bool UnmodifiableObjectWarnings { get; set; } = true;
 
         public bool TreatVerificationErrorsAsWarnings { get; set; }
-
-        [DefaultValue(true)]
+        
         public bool ScriptRefreshModule { get; set; } = true;
-
-        [DefaultValue(true)]
+        
         public bool ScriptNewConstraintValidation { get; set; } = true;
 
         public bool ScriptFileSize { get; set; }
@@ -94,8 +81,7 @@ namespace Microsoft.SqlTools.ServiceLayer.SchemaCompare.Contracts
         public bool RunDeploymentPlanExecutors { get; set; }
 
         public bool RegisterDataTierApplication { get; set; }
-
-        [DefaultValue(true)]
+        
         public bool PopulateFilesOnFileGroups { get; set; } = true;
 
         public bool NoAlterStatementsToChangeClrTypes { get; set; }
@@ -107,23 +93,17 @@ namespace Microsoft.SqlTools.ServiceLayer.SchemaCompare.Contracts
         public bool AllowUnsafeRowLevelSecurityDataMovement { get; set; }
 
         public bool IgnoreWithNocheckOnCheckConstraints { get; set; }
-
-        [DefaultValue(true)]
+        
         public bool IgnoreFillFactor { get; set; } = true;
-
-        [DefaultValue(true)]
+        
         public bool IgnoreFileSize { get; set; } = true;
-
-        [DefaultValue(true)]
+        
         public bool IgnoreFilegroupPlacement { get; set; } = true;
-
-        [DefaultValue(true)]
+        
         public bool DoNotAlterReplicatedObjects { get; set; } = true;
-
-        [DefaultValue(true)]
+        
         public bool DoNotAlterChangeDataCaptureObjects { get; set; } = true;
-
-        [DefaultValue(true)]
+        
         public bool DisableAndReenableDdlTriggers { get; set; } = true;
 
         public bool DeployDatabaseInSingleUserMode { get; set; }
@@ -133,13 +113,11 @@ namespace Microsoft.SqlTools.ServiceLayer.SchemaCompare.Contracts
         public bool CompareUsingTargetCollation { get; set; }
 
         public bool CommentOutSetVarDeclarations { get; set; }
-
-        [DefaultValue(120)]
+        
         public int CommandTimeout { get; set; } = 120;
 
         public bool BlockWhenDriftDetected { get; set; }
-
-        [DefaultValue(true)]
+        
         public bool BlockOnPossibleDataLoss { get; set; } = true;
 
         public bool BackupDatabaseBeforeChanges { get; set; }
@@ -148,23 +126,18 @@ namespace Microsoft.SqlTools.ServiceLayer.SchemaCompare.Contracts
 
         public bool AllowDropBlockingAssemblies { get; set; }
 
-        public string AdditionalDeploymentContributorArguments { get; set; } 
+        public string AdditionalDeploymentContributorArguments { get; set; }
 
         public string AdditionalDeploymentContributors { get; set; }
-
-        [DefaultValue(true)]
+        
         public bool DropConstraintsNotInSource { get; set; } = true;
-
-        [DefaultValue(true)]
+        
         public bool DropDmlTriggersNotInSource { get; set; } = true;
-
-        [DefaultValue(true)]
+        
         public bool DropExtendedPropertiesNotInSource { get; set; } = true;
-
-        [DefaultValue(true)]
+        
         public bool DropIndexesNotInSource { get; set; } = true;
-
-        [DefaultValue(true)]
+        
         public bool IgnoreFileAndLogFilePath { get; set; } = true;
 
         public bool IgnoreExtendedProperties { get; set; }
@@ -178,11 +151,9 @@ namespace Microsoft.SqlTools.ServiceLayer.SchemaCompare.Contracts
         public bool IgnoreDdlTriggerState { get; set; }
 
         public bool IgnoreDdlTriggerOrder { get; set; }
-
-        [DefaultValue(true)]
+        
         public bool IgnoreCryptographicProviderFilePath { get; set; } = true;
-
-        [DefaultValue(true)]
+        
         public bool VerifyDeployment { get; set; } = true;
 
         public bool IgnoreComments { get; set; }
@@ -190,20 +161,17 @@ namespace Microsoft.SqlTools.ServiceLayer.SchemaCompare.Contracts
         public bool IgnoreColumnCollation { get; set; }
 
         public bool IgnoreAuthorizer { get; set; }
-
-        [DefaultValue(true)]
+        
         public bool IgnoreAnsiNulls { get; set; } = true;
 
         public bool GenerateSmartDefaults { get; set; }
-
-        [DefaultValue(true)]
+        
         public bool DropStatisticsNotInSource { get; set; } = true;
 
         public bool DropRoleMembersNotInSource { get; set; }
 
         public bool DropPermissionsNotInSource { get; set; }
-
-        [DefaultValue(true)]
+        
         public bool DropObjectsNotInSource { get; set; } = true;
 
         public bool IgnoreColumnOrder { get; set; }
