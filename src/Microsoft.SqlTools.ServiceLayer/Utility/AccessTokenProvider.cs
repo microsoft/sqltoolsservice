f7a7a3d4664eea56119544c12492bb8d2bb43f12
Kim Santiago <31145923+kisantia@users.noreply.github.com>
2019-07-24 17:23:55 -0700

    Fix DacFx wizard not supporting AAD auth (#836)
    * pass Azure authentication token if using AAD auth
    
    * add check for null

---
+++ src/Microsoft.SqlTools.ServiceLayer/Utility/AccessTokenProvider.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.SqlServer.Dac;
+using System;
+
+namespace Microsoft.SqlTools.ServiceLayer.Utility
+{
+    class AccessTokenProvider : IUniversalAuthProvider
+    {
+        private string _accessToken;
+
+        public AccessTokenProvider(string accessToken)
+        {
+            if (string.IsNullOrEmpty(accessToken))
+            {
+                throw new ArgumentNullException("accessToken");
+            }
+
+            _accessToken = accessToken;
+        }
+
+        public bool IsTokenExpired() { return false; }
+
+        public string GetValidAccessToken() { return _accessToken; }
+    }
+}
