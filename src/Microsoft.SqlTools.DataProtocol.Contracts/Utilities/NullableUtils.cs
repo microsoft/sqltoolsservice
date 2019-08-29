c6e3b33c3503a0e4ac14620340098d13b0e00603
sergei-boguslavski <54648384+sergei-boguslavski@users.noreply.github.com>
2019-08-29 10:35:45 -0700

    add flag converters nullable serialization (#857)

---
+++ src/Microsoft.SqlTools.DataProtocol.Contracts/Utilities/NullableUtils.cs
@@ -0,0 +1,36 @@
+﻿//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+using System;
+
+namespace Microsoft.SqlTools.DataProtocol.Contracts.Utilities
+{
+    internal static class NullableUtils
+    {
+        /// <summary>
+        /// Determines whether the type is <see cref="Nullable{T}"/>.
+        /// </summary>
+        /// <param name="t">The type.</param>
+        /// <returns>
+        ///   <c>true</c> if <paramref name="t"/> is <see cref="Nullable{T}"/>; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool IsNullable(Type t)
+        {
+            return t.IsGenericType && t.GetGenericTypeDefinition() == typeof(Nullable<>);
+        }
+
+        /// <summary>
+        /// Unwraps the <see cref="Nullable{T}"/> if necessary and returns the underlying value type.
+        /// </summary>
+        /// <param name="t">The type.</param>
+        /// <returns>The underlying value type the <see cref="Nullable{T}"/> type was produced from,
+        /// or the <paramref name="t"/> type if the type is not <see cref="Nullable{T}"/>.
+        /// </returns>
+        public static Type GetUnderlyingTypeIfNullable(Type t)
+        {
+            return IsNullable(t) ? Nullable.GetUnderlyingType(t) : t;
+        }
+    }
+}
