d4f6ee607ccd8aea2cfc27f09ddda1d27f541cd2
Karl Burtram <karlb@microsoft.com>
2019-12-19 12:55:37 -0800

    Remove auth from CoreServices too

--- src/Microsoft.SqlTools.CoreServices/Connection/ConnectionServiceCore.cs
+++ src/Microsoft.SqlTools.CoreServices/Connection/ConnectionServiceCore.cs
@@ -1023,9 +1023,10 @@ namespace Microsoft.SqlTools.CoreServices.Connection
                         break;
                     case "SqlLogin":
                         break;
-                    case "ActiveDirectoryPassword":
-                        connectionBuilder.Authentication = SqlAuthenticationMethod.ActiveDirectoryPassword;
-                        break;
+                    // turn off for release/1.14 - karlb 12/19
+					//case "ActiveDirectoryPassword":
+                    //    connectionBuilder.Authentication = SqlAuthenticationMethod.ActiveDirectoryPassword;
+                    //    break;
                     default:
                         throw new ArgumentException(SR.ConnectionServiceConnStringInvalidAuthType(connectionDetails.AuthenticationType));
                 }
