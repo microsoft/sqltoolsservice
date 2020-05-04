9b0fa8c5c460b84fc75c128d0cb164360d9e7ca4
Jeff Trimmer <jetrimme@microsoft.com>
2020-05-04 13:08:47 -0700

    Remove unused TelemetryMessage object

--- src/Microsoft.SqlTools.ServiceLayer/AutoParameterizaition/Exceptions/ParameterizationFormatException.cs
+++ src/Microsoft.SqlTools.ServiceLayer/AutoParameterizaition/Exceptions/ParameterizationFormatException.cs
@@ -9,33 +9,30 @@ using Microsoft.SqlTools.ServiceLayer.AutoParameterizaition.Helpers;
 namespace Microsoft.SqlTools.ServiceLayer.AutoParameterizaition.Exceptions
 {
     /// <summary>
-    /// ParameterizationFormatException is used to surface format exceptions encountered in the TSQL batch to perform auto-parameterization of literals
+    /// ParameterizationFormatException is used to surface format exceptions encountered in the TSQL batch to perform 
+    /// auto-parameterization of literals for Always Encrypted.
     /// </summary>
-    class ParameterizationFormatException : FormatException
+    public class ParameterizationFormatException : FormatException
     {
         public readonly int LineNumber;
         public readonly string LiteralValue;
         public readonly string CodeSenseMessage;
         public readonly string LogMessage;
-        public readonly string TelemetryMessage;
         public readonly string SqlDatatype;
         public readonly string CSharpDataType;
 
         public ParameterizationFormatException(MessageHelper.MessageType type, string variableName, string sqlDataType, string cSharpDataType, string literalValue, int lineNumber)
-            : this(type, variableName, sqlDataType, cSharpDataType, literalValue, lineNumber, null)
-        {
-        }
+            : this(type, variableName, sqlDataType, cSharpDataType, literalValue, lineNumber, exception: null) { }
 
-        public ParameterizationFormatException(MessageHelper.MessageType type, string variableName, string sqlDataType, string cSharpDataType, string literalValue, int lineNumber, Exception e)
-            : base(MessageHelper.GetLocalizedMessage(type, variableName, sqlDataType, literalValue), e)
+        public ParameterizationFormatException(MessageHelper.MessageType type, string variableName, string sqlDataType, string cSharpDataType, string literalValue, int lineNumber, Exception exception)
+            : base(MessageHelper.GetLocalizedMessage(type, variableName, sqlDataType, literalValue), exception)
         {
             LineNumber = lineNumber;
             LiteralValue = literalValue;
             SqlDatatype = sqlDataType;
             CSharpDataType = cSharpDataType;
-            CodeSenseMessage = this.Message;
+            CodeSenseMessage = Message;
             LogMessage = MessageHelper.GetLocaleInvariantMessage(type, variableName, sqlDataType) + ", Literal Value: " + literalValue + ", On line: " + lineNumber + ", CSharp DataType: " + cSharpDataType;
-            TelemetryMessage = MessageHelper.GetLocaleInvariantMessage(type, variableName, sqlDataType);
         }
     }
 }
