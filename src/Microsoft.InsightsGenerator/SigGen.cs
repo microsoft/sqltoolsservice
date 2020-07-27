373a9d87facc4848a117148657968f56368ae864
Aasim Khan <aasimkhan30@gmail.com>
2020-07-27 14:16:52 -0700

    Setting up siggen class

---
+++ src/Microsoft.InsightsGenerator/SigGen.cs
@@ -0,0 +1,23 @@
+namespace Microsoft.InsightsGenerator
+{
+    class SignatureGenerator
+    {
+        private DataArray table;
+        private SignatureGeneratorResult results;
+        public SignatureGenerator(DataArray table)
+        {
+            this.table = table;
+        }
+        
+        public Learn()
+        {
+
+        }
+    }
+}
+
+
+public class SignatureGeneratorResult
+{
+    public Dictionary<string, string[]> results;
+}
