665a6c000a3d7fab5788ae446342eebeed5afec5
Karl Burtram <karlb@microsoft.com>
2020-07-28 13:18:09 -0700

    More tests and bug fix

--- src/Microsoft.InsightsGenerator/DataTransformation.cs
+++ src/Microsoft.InsightsGenerator/DataTransformation.cs
@@ -72,9 +72,9 @@ namespace Microsoft.InsightsGenerator
                     ++startingIndex;
                 }
 
-                for (int i = startingIndex; i < stringColumns.Count; ++i)
+                for (int i = 0; i < stringColumns.Count - startingIndex; ++i)
                 {
-                    labels[stringColumns[i].ColumnIndex] = "slicer_" + i;
+                    labels[stringColumns[i + startingIndex].ColumnIndex] = "slicer_" + i;
                 }
             }
 
