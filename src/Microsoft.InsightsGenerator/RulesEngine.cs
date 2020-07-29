640e0fa3805009fd864949f01b64c11d61a8aec0
jiarima <68882862+jiarima@users.noreply.github.com>
2020-07-29 13:53:56 -0700

    Added all the templates (#1015)
    * Added all the templates
    Added a method to find matched template
    
    * Added a function to replace # and ## values in a template
    
    * Added ReplaceHashesInTemplate call
    
    * Added comments
    
    * Updated the template txt
    
    * Updated GetTopHeadersWithHash function to add #toplist
    
    * Updated the logic per offline discussion with Hermineh

--- src/Microsoft.InsightsGenerator/RulesEngine.cs
+++ src/Microsoft.InsightsGenerator/RulesEngine.cs
@@ -3,81 +3,208 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 //
 
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace Microsoft.InsightsGenerator
 {
     public class RulesEngine
     {
-        public class ColumnHeaders
+        public static List<Template> Templates;
+
+        public static List<string> TopListHashHeaders = new List<string>{ "#top", "#averageSlice", "#topPerslice" , "#bottom"};
+
+        public RulesEngine()
         {
-            public List<string> SingleHashValues { get; set; }
-            public List<string> DoubleHashValues { get; set; }
-            public string Template { get; set; }
+            Templates = GetTemplates();
         }
 
-
-        public static ColumnHeaders TemplateParser(string templateFile)
+        public static ColumnHeaders TemplateParser(string templateContent)
         {
-            StreamReader file = new StreamReader($"{templateFile}");
-            string line = null;
-            string templateText = null;
+
             ColumnHeaders ch = new ColumnHeaders();
-            ch.SingleHashValues = new List<string>();
-            ch.DoubleHashValues = new List<string>();
-            while (!file.EndOfStream)
+            var processedText = Regex.Replace(templateContent, @",|\\n", "");
+            ch.Template = templateContent;
+
+            List<string> keyvalue = processedText.Split(' ').Select(s => s.Trim()).ToList();
+
+            foreach (string s in keyvalue)
             {
-                //line = file.ReadLine().Replace("\\n",string.Empty);
-                line = Regex.Replace(file.ReadLine(), @",|\\n", "");
-                templateText = line;
-                ch.Template = templateText;
-                List<string> keyvalue = line.Split(' ').Select(s => s.Trim()).ToList();
-                foreach (string s in keyvalue)
+                if (s.StartsWith("#"))
                 {
-                    if (s.StartsWith("#"))
+                    string headers = s.Substring(1, s.Length - 1);
+                    if (headers.StartsWith("#"))
                     {
-                        string headers = s.Substring(1, s.Length - 1);
-                        if (headers.StartsWith("#"))
-                        {
-                            ch.DoubleHashValues.Add(headers.Substring(1, headers.Length - 1));
-                        }
-                        else
-                        {
-                            ch.SingleHashValues.Add(headers);
-                        }
+                        ch.DoubleHashValues.Add(headers.Substring(1, headers.Length - 1));
                     }
-                    if (s.Contains("tempId"))
+                    else
                     {
-                        Debug.WriteLine(s);
+                        ch.SingleHashValues.Add(headers);
                     }
                 }
-
             }
-
             return ch;
         }
 
-        public static Dictionary<int, string> RulesGeneratorFromTemplate()
+        /// <summary>
+        /// Find a matched template based on the single hash values
+        /// </summary>
+        /// <param name="singleHashHeaders"></param>
+        /// <returns></returns>
+        public static string FindMatchedTemplate(List<List<string>> singleHashHeaders, DataArray columnInfo)
         {
-            Dictionary<int, string> Rules_templateID = new Dictionary<int, string>();
-            string rules = null;
-            ColumnHeaders header = TemplateParser(@"template_16.txt");
-            return Rules_templateID;
+            var resultTemplate = new StringBuilder();
+            if (Templates == null)
+            {
+                Templates = GetTemplates();
+            }
+            var headersWithSingleHash = GetTopHeadersWithHash(singleHashHeaders);
+
+            foreach (var template in Templates)
+            {
+                var columnHeaders = TemplateParser(template.Content);
+                var singleHashFromTemplate = columnHeaders.SingleHashValues;
+
+                var isMatched = true;
+
+                // all the values from template needs to be found in the input from SigGen
+                foreach (var hashFromTemplate in singleHashFromTemplate)
+                {
+                    if (!headersWithSingleHash.Contains(hashFromTemplate))
+                    {
+                        isMatched = false;
+                    }
+                }
+                if (isMatched)
+                {
+                    // Replace # and ## values in template with actual values
+                    ReplaceHashesInTemplate(singleHashHeaders, columnInfo, template);
+                    resultTemplate.AppendLine(template.Content + "\n");
+                }  
+            }
+
+            // No matched Template found
+            return resultTemplate.ToString();
         }
 
-        public static string RulesChecking(string singleHashHeaders, string doublehashHeaders)
+        private static Template ReplaceHashesInTemplate(List<List<string>>singleHashList, DataArray columnInfo, Template template)
         {
-            string template = null;
-            //if(singleHashHeaders!=null && doublehashHeaders!=null && RulesGeneratorFromInput(singleHashHeaders, doublehashHeaders).Equals(RulesGeneratorFromTemplate()){
-            //from dictionary mapping select template id
-            //}
+            StringBuilder modifiedTemp = new StringBuilder(template.Content);
 
+            // Replace single hash values
+            foreach (var line in singleHashList)
+            {
+                // Example of a single list (line): 
+                // "top", "3", " input (value) %OfValue ", " input (value) %OfValue ", " input (value) %OfValue "
+                var headerInputs = line.ToArray();
+                string header = "#" + headerInputs[0];
+
+                if (TopListHashHeaders.Contains(header))
+                {
+                    //First replace the header with the second value in the list
+
+                    modifiedTemp.Replace(header, headerInputs[1]);
+                    StringBuilder topListStr = new StringBuilder();
+                    for (int i = 2; i < headerInputs.Length - 1; i++)
+                    {
+                        // Append all the rest of the elemet in the array separated by new line
+                        topListStr.AppendLine(headerInputs[i]);
+                    }
+                    modifiedTemp.Replace("#placeHolder", topListStr.ToString());
+                }
+                else 
+                {
+                    modifiedTemp.Replace("#" + headerInputs[0], headerInputs[1]);
+                }
+            }
+
+            // Replace double hash values
+            var transformedColumnArray = columnInfo.TransformedColumnNames.ToArray();
+            var columnArray = columnInfo.ColumnNames.ToArray();
+
+            for (int p = 0; p < columnInfo.TransformedColumnNames.Length - 1; p++)
+            {
+                modifiedTemp.Replace("#" + transformedColumnArray[p], columnArray[p]);
+            }
+
+            template.Content = modifiedTemp.ToString();
             return template;
         }
+
+        private static List<string> GetTopHeadersWithHash(List<List<string>> singleHashHeaders)
+        {
+            var topHeaderList = new List<string>();
+            foreach (var list in singleHashHeaders)
+            {
+                topHeaderList.Add("#" + list.First());
+                if (TopListHashHeaders.Contains("#" + list.First()))
+                {
+                    topHeaderList.Add("#" + "placeHolder");
+                }
+            }
+            return topHeaderList;
+        }
+
+        /// <summary>
+        /// Retrieve all the templates and template ids
+        /// </summary>
+        /// <returns>All the template & id comnbination </returns>
+        public static List<Template> GetTemplates()
+        {
+            var templateHolder = new List<Template>();
+
+            int templateId;
+            using (StreamReader streamReader = new StreamReader($"Templates/templates.txt", Encoding.UTF8))
+            {
+                int temId = 0;
+                var wholeText = streamReader.ReadToEnd();
+                var templateArray = wholeText.Split(new[] { "Template " }, StringSplitOptions.None).ToList();
+                
+                foreach (var line in templateArray.Where(r => r != string.Empty))
+                {
+                    var parts = line.Split(new[] { "\r\n" }, StringSplitOptions.None).ToList();
+                    
+                        temId = int.Parse(parts[0]);
+                        templateHolder.Add(new Template(temId, parts[1]));
+                }
+
+                return templateHolder;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Template container to hold the template id + template body combination
+    /// </summary>
+    public class Template
+    {
+        public Template(int id, string content)
+        {
+            Id = id;
+            Content = content;
+        }
+
+        public int Id { get; set; }
+        public string Content { get; set; }
+    }
+
+    public class ColumnHeaders
+    {
+        public ColumnHeaders()
+        {
+            SingleHashValues = new List<string>();
+            DoubleHashValues = new List<string>();
+            Template = null;
+        }
+
+        public List<string> SingleHashValues { get; set; }
+        public List<string> DoubleHashValues { get; set; }
+        public string Template { get; set; }
     }
 }
 
--- test/Microsoft.InsightsGenerator.UnitTests/RulesEngineTests.cs
+++ test/Microsoft.InsightsGenerator.UnitTests/RulesEngineTests.cs
@@ -3,7 +3,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 //
 
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using Xunit;
@@ -12,20 +11,26 @@ using static Microsoft.InsightsGenerator.RulesEngine;
 namespace Microsoft.InsightsGenerator.UnitTests
 {
     /// <summary>
-    /// DataTransformation tests
+    /// Rules Engine tests
     /// </summary>
     public class RulesEngineTests
     {
         [Fact]
         public void TemplateParserTest()
         {
-            ColumnHeaders header = TemplateParser(@"TestTemplates/template_16.txt");
-            var expectedSingleHashValues = new List<string>(new string[] {"slices", "time", "stHData", "Etime", "ESlices", "EstHData"  });
-            var expectedDoubleHashValues = new List<string>(new string[] { "SlicePar_GG_1(s)", "OutPar_N_C_1", "SlicePar_GG_1" });
+            ColumnHeaders headerForTemp8 = TemplateParser(@"#inp had a total of #OutPar_N_C_1 ##OutPar_N_C_1 that constitues #Per% \n \n");
+            ColumnHeaders headerForTemp16 = TemplateParser(@"For the #slices ##SlicePar_GG_1(s), the percentage of ##OutPar_N_C_1 on #time were \n #stHData\n this was compared with #Etime where #ESlices ##SlicePar_GG_1\n #EstHData  \n.");
 
-            Assert.True(Enumerable.SequenceEqual(expectedSingleHashValues, header.SingleHashValues));
-            Assert.True(Enumerable.SequenceEqual(expectedDoubleHashValues, header.DoubleHashValues));
+            var expectedSingleHashValuesForTemp16 = new List<string>(new string[] {"slices", "time", "stHData", "Etime", "ESlices", "EstHData"  });
+            var expectedDoubleHashValuesForTemp16 = new List<string>(new string[] { "SlicePar_GG_1(s)", "OutPar_N_C_1", "SlicePar_GG_1" });
+
+            var expectedSingleHashValuesForTemp8 = new List<string>(new string[] { "inp", "OutPar_N_C_1", "Per%" });
+            var expectedDoubleHashValuesForTemp8 = new List<string>(new string[] { "OutPar_N_C_1" });
+      
+            Assert.True(Enumerable.SequenceEqual(expectedSingleHashValuesForTemp8, headerForTemp8.SingleHashValues));
+            Assert.True(Enumerable.SequenceEqual(expectedDoubleHashValuesForTemp8, headerForTemp8.DoubleHashValues));
+            Assert.True(Enumerable.SequenceEqual(expectedSingleHashValuesForTemp16, headerForTemp16.SingleHashValues));
+            Assert.True(Enumerable.SequenceEqual(expectedDoubleHashValuesForTemp16, headerForTemp16.DoubleHashValues));
         }
     }
 }
-
