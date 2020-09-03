aa80bd944fc27d5eb9a7614f50b8fef12c2a5cb8
Justin M <63619224+JustinMDotNet@users.noreply.github.com>
2020-09-03 16:17:39 -0700

    3278 Kusto Unit Tests - Part 2 (#1063)
    * 3278 Added unit tests in MetadataFactoryTests and Microsoft.Kusto.ServiceLayer.UnitTests project
    
    * 3278 Removed todo and changed unit test to validate megabytes
    
    * 3278 Added file and unit tests in AutoCompleteHelperTests.cs
    
    * 3278 Removed unused functions from Kusto > ScriptAsScriptingOperation
    
    * 3278 Added unit tests for DataSourceFactory
    
    * 3278 Refactored AdminService to pass in ConnectionService rather than through instance variable. Added unit test for AdminServiceTests
    
    * 3278 Refactored DataSourceFactory to not have static functions for future unit tests
    
    * 3278 Re-added properties that were flagged as unused but are being used by ADS in ReliableDataSourceConnection.cs
    
    * 3278 Re-added properties that were flagged as unused but are being used by ADS in ReliableDataSourceConnection.cs
    
    * adding pipeline to execute tests (#1062)
    
    * 3278 Converted GetDefaultAutoComplete and GetDefaultSemanticMarkers to static functions in DataSourceFactory. Removed unused constructor in ScriptFile. Added positive unit tests for both functions
    
    * undoing release version bump
    
    * adding additional configs
    
    * 3278 Minor refactors in ConnectionInfo, BindingQueue, DiagnosticsHelper, MetadataService, and HostLoader. Changed AssemblyInfo to only allow Kusto Unit Tests for internal access. Added lots of unit tests.
    
    * 3278 Commented out bindingContext.IsConnected in AddConnectionContext_Sets_BindingContext
    
    * 3278 Reversed order of unit tests in ConnectedBindingQueueTests and added throw to Catch block.
    
    * 3278 Reverted change to ConnectedBindingQueue. Removed unit test from AddConnectionContext for NeedsMetaData True
    
    Co-authored-by: Jorge Berumen <52225468+joberume@users.noreply.github.com>
    Co-authored-by: joberume <jberumen3@miners.utep.edu>

--- src/Microsoft.Kusto.ServiceLayer/Properties/AssemblyInfo.cs
+++ src/Microsoft.Kusto.ServiceLayer/Properties/AssemblyInfo.cs
@@ -41,10 +41,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: AssemblyInformationalVersion("1.0.0.0")]
 
-[assembly: InternalsVisibleTo("Microsoft.SqlTools.ServiceLayer.UnitTests")]
-[assembly: InternalsVisibleTo("Microsoft.SqlTools.ServiceLayer.IntegrationTests")]
-[assembly: InternalsVisibleTo("Microsoft.SqlTools.ManagedBatchParser.UnitTests")]
-[assembly: InternalsVisibleTo("Microsoft.SqlTools.ServiceLayer.Test.Common")]
+[assembly: InternalsVisibleTo("Microsoft.Kusto.ServiceLayer.UnitTests")]
 
 // Allowing internals visible access to Moq library to help testing
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
