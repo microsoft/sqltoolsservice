beaa5d78e140cf0120698706abc9cdf91bcb5f91
Karl Burtram <karlb@microsoft.com>
2020-09-03 14:13:26 -0700

    Remove kustoservicelayer from SQL Tools Service load

--- src/Microsoft.SqlTools.ServiceLayer/HostLoader.cs
+++ src/Microsoft.SqlTools.ServiceLayer/HostLoader.cs
@@ -67,7 +67,11 @@ namespace Microsoft.SqlTools.ServiceLayer
         {
             // Load extension provider, which currently finds all exports in current DLL. Can be changed to find based
             // on directory or assembly list quite easily in the future
-            ExtensionServiceProvider serviceProvider = ExtensionServiceProvider.CreateDefaultServiceProvider();
+            ExtensionServiceProvider serviceProvider = ExtensionServiceProvider.CreateDefaultServiceProvider(new string[] {
+                "microsofsqltoolscredentials.dll",
+                "microsoft.sqltools.hosting.dll",
+                "microsoftsqltoolsservicelayer.dll"
+            });
             serviceProvider.RegisterSingleService(sqlToolsContext);
             serviceProvider.RegisterSingleService(serviceHost);
 
