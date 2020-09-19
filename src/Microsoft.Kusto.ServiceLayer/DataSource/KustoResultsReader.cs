ea04681726e67fe1090fdf7c8946fc8dfc0a64e7
Shafiq Rahman <srahman@microsoft.com>
2020-09-19 02:09:11 -0700

    Revert "Fix for displaying multiple table (#1075)"
    This reverts commit df0be310198de1921f4acc1565533f70eb859938.

--- src/Microsoft.Kusto.ServiceLayer/DataSource/KustoResultsReader.cs
+++ src/Microsoft.Kusto.ServiceLayer/DataSource/KustoResultsReader.cs
@@ -1,27 +1,21 @@
-﻿using Microsoft.Kusto.ServiceLayer.QueryExecution;
-using System.Collections.Generic;
-using System.Data;
+﻿using System.Data;
 
 namespace Microsoft.Kusto.ServiceLayer.DataSource
 {
     internal class KustoResultsReader : DataReaderWrapper
     {
-        public KustoResultsReader(IDataReader reader)
-            : base(reader)
+        public KustoResultsReader(IDataReader reader) : base(reader)
         {
         }
-
+        
         /// <summary>
-        /// Kusto returns atleast 4 results tables - QueryResults(sometimes more than one), QueryProperties, QueryStatus and Query Results Metadata Table. 
-        /// When returning query results we need to trim off the last 3 tables as we want the caller to only read results table. 
-        /// </summary> 
-
-        public void SanitizeResults(List<ResultSet> resultSets)
-        {
-            if (resultSets.Count > 3)
-            {
-                resultSets.RemoveRange(resultSets.Count - 3, 3);
-            }
+        /// Kusto returns 3 results tables - QueryResults, QueryProperties, QueryStatus. When returning query results	
+        /// we want the caller to only read the first table. We override the NextResult function here to only return one table	
+        /// from the IDataReader.	
+        /// </summary>	
+        public override bool NextResult()	
+        {	
+            return false;	
         }
     }
 }
