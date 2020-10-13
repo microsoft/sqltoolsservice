1a0388c8cbf0226bcaf88a6c0e975ad251e72064
Karl Burtram <karlb@microsoft.com>
2020-10-13 11:02:29 -0700

    Initial migration service changes for assessment support (#1093)
    * Migration service WIP 1
    
    * Assessment DLLs
    
    * WIP
    
    * WIP 2
    
    * Add migration nuget package
    
    * WIP2
    
    * Fix up assessment handler
    
    * WIP
    
    * Fix build scripts
    
    * WIP
    
    * Update Assessment references
    
    * Fix metadata copy to work with publish

---
+++ test/Microsoft.SqlTools.ServiceLayer.IntegrationTests/Migration/MigrationServiceTests.cs
@@ -0,0 +1,40 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+using System.Threading.Tasks;
+using Microsoft.SqlTools.Hosting.Protocol;
+using Microsoft.SqlTools.ServiceLayer.IntegrationTests.Utility;
+using Microsoft.SqlTools.ServiceLayer.Migration;
+using Microsoft.SqlTools.ServiceLayer.Migration.Contracts;
+using Microsoft.SqlTools.ServiceLayer.SqlAssessment.Contracts;
+using Microsoft.SqlTools.ServiceLayer.Test.Common;
+using Moq;
+using NUnit.Framework;
+
+namespace Microsoft.SqlTools.ServiceLayer.IntegrationTests.Migration
+{
+    public class MigrationgentServiceTests
+    {
+        [Test]
+        public async Task TestHandleMigrationAssessmentRequest()
+        {
+            using (SelfCleaningTempFile queryTempFile = new SelfCleaningTempFile())
+            {
+                var connectionResult = await LiveConnectionHelper.InitLiveConnectionInfoAsync("master", queryTempFile.FilePath);
+
+                var requestParams = new MigrationAssessmentsParams()
+                {
+                    OwnerUri = connectionResult.ConnectionInfo.OwnerUri
+                };
+
+                var requestContext = new Mock<RequestContext<MigrationAssessmentResult>>();
+
+                MigrationService service = new MigrationService();
+                await service.HandleMigrationAssessmentsRequest(requestParams, requestContext.Object);
+                requestContext.VerifyAll();
+            }     
+        }
+    }
+}
