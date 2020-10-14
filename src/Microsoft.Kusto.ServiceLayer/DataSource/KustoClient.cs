1ce082ad6ee750c0fe7cca0a76c7822028e1693c
Justin M <63619224+JustinMDotNet@users.noreply.github.com>
2020-10-14 16:21:08 -0700

    3875 Changed ExecuteQueryAsync and ExecuteControlCommandAsync to return correct types in IKustoClient. Added RetryLimit to KustoClient. Added check to skip database and function schema when no databaseName available. Added logic for retry on ExecuteQuery and ExecuteControlCommand

--- src/Microsoft.Kusto.ServiceLayer/DataSource/KustoClient.cs
+++ src/Microsoft.Kusto.ServiceLayer/DataSource/KustoClient.cs
@@ -38,6 +38,8 @@ namespace Microsoft.Kusto.ServiceLayer.DataSource
         public string ClusterName { get; }
         public string DatabaseName { get; private set; }
 
+        private const int RetryLimit = 60;
+
         public KustoClient(string connectionString, string azureAccountToken, string ownerUri)
         {
             _ownerUri = ownerUri;
@@ -49,21 +51,23 @@ namespace Microsoft.Kusto.ServiceLayer.DataSource
 
         private GlobalState LoadSchemaState()
         {
-            CancellationTokenSource source = new CancellationTokenSource();
-
             IEnumerable<ShowDatabaseSchemaResult> tableSchemas = Enumerable.Empty<ShowDatabaseSchemaResult>();
             IEnumerable<ShowFunctionsResult> functionSchemas = Enumerable.Empty<ShowFunctionsResult>();
 
-            Parallel.Invoke(() =>
+            if (!string.IsNullOrWhiteSpace(DatabaseName))
             {
-                tableSchemas = ExecuteControlCommandAsync<ShowDatabaseSchemaResult>(
-                    $".show database {DatabaseName} schema",
-                    false, source.Token).Result;
-            }, () =>
-            {
-                functionSchemas = ExecuteControlCommandAsync<ShowFunctionsResult>(".show functions", false,
-                    source.Token).Result;
-            });
+                var source = new CancellationTokenSource();
+                Parallel.Invoke(() =>
+                {
+                    tableSchemas =
+                        ExecuteQueryAsync<ShowDatabaseSchemaResult>($".show database {DatabaseName} schema",
+                            source.Token, DatabaseName).Result;
+                }, () =>
+                {
+                    functionSchemas =
+                        ExecuteQueryAsync<ShowFunctionsResult>(".show functions", source.Token, DatabaseName).Result;
+                });
+            }
 
             return KustoIntellisenseHelper.AddOrUpdateDatabase(tableSchemas, functionSchemas,
                 GlobalState.Default,
@@ -165,16 +169,18 @@ namespace Microsoft.Kusto.ServiceLayer.DataSource
                 {
                     var minimalQuery =
                         codeBlock.Service.GetMinimalText(MinimalTextKind.RemoveLeadingWhitespaceAndComments);
-                    
+
                     IDataReader origReader;
-                
-                    if(minimalQuery.StartsWith(".") && !minimalQuery.StartsWith(".show")){
+
+                    if (minimalQuery.StartsWith(".") && minimalQuery.StartsWith(".show"))
+                    {
                         origReader = _kustoAdminProvider.ExecuteControlCommand(
                             KustoQueryUtils.IsClusterLevelQuery(minimalQuery) ? "" : databaseName,
                             minimalQuery,
                             clientRequestProperties);
                     }
-                    else{
+                    else
+                    {
                         origReader = _kustoQueryProvider.ExecuteQuery(
                             KustoQueryUtils.IsClusterLevelQuery(minimalQuery) ? "" : databaseName,
                             minimalQuery,
@@ -183,16 +189,24 @@ namespace Microsoft.Kusto.ServiceLayer.DataSource
 
                     origReaders[index] = origReader;
                 });
-                
+
                 return new KustoResultsReader(origReaders);
             }
-            catch (AggregateException exception) 
-                when (retryCount > 0 &&
-                      exception.InnerException is KustoRequestException innerException 
+            catch (AggregateException exception)
+                when (retryCount < RetryLimit &&
+                      exception.InnerException is KustoRequestException innerException
                       && innerException.FailureCode == 401) // Unauthorized
             {
+                Thread.Sleep(retryCount * 1000);
+                retryCount *= 2;
                 RefreshAzureToken();
-                retryCount--;
+                return ExecuteQuery(query, cancellationToken, databaseName, retryCount);
+            }
+            catch (AggregateException exception) when (retryCount < RetryLimit 
+                                                       && exception.InnerException is KustoRequestException)
+            {
+                Thread.Sleep(retryCount * 1000);
+                retryCount *= 2;
                 return ExecuteQuery(query, cancellationToken, databaseName, retryCount);
             }
         }
@@ -200,19 +214,15 @@ namespace Microsoft.Kusto.ServiceLayer.DataSource
         /// <summary>
         /// Executes a query or command against a kusto cluster and returns a sequence of result row instances.
         /// </summary>
-        public async Task<IEnumerable<T>> ExecuteControlCommandAsync<T>(string command, bool throwOnError,
+        public async Task ExecuteControlCommandAsync(string command, bool throwOnError,
             CancellationToken cancellationToken)
         {
             try
             {
-                var resultReader = await ExecuteQueryAsync(command, cancellationToken, DatabaseName);
-                var results = KustoDataReaderParser.ParseV1(resultReader, null);
-                var tableReader = results[WellKnownDataSet.PrimaryResult].Single().TableData.CreateDataReader();
-                return new ObjectReader<T>(tableReader);
+                await Task.Run(() => ExecuteControlCommand(command), cancellationToken);
             }
             catch (Exception) when (!throwOnError)
             {
-                return null;
             }
         }
 
@@ -221,11 +231,20 @@ namespace Microsoft.Kusto.ServiceLayer.DataSource
         /// </summary>
         /// <param name="query">The query.</param>
         /// <returns>The results.</returns>
-        public Task<IDataReader> ExecuteQueryAsync(string query, CancellationToken cancellationToken,
+        public async Task<IEnumerable<T>> ExecuteQueryAsync<T>(string query, CancellationToken cancellationToken,
             string databaseName = null)
         {
-            var reader = ExecuteQuery(query, cancellationToken, databaseName);
-            return Task.FromResult(reader);
+            try
+            {
+                var resultReader = await Task.Run(() => ExecuteQuery(query, cancellationToken, databaseName), cancellationToken);
+                var results = KustoDataReaderParser.ParseV1(resultReader, null);
+                var tableReader = results[WellKnownDataSet.PrimaryResult].Single().TableData.CreateDataReader();
+                return new ObjectReader<T>(tableReader);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
         }
 
         private void CancelQuery(string clientRequestId)
@@ -249,10 +268,18 @@ namespace Microsoft.Kusto.ServiceLayer.DataSource
                 {
                 }
             }
-            catch (KustoRequestException exception) when (retryCount > 0 && exception.FailureCode == 401) // Unauthorized
+            catch (KustoRequestException exception) when (retryCount < RetryLimit && exception.FailureCode == 401
+            ) // Unauthorized
             {
+                Thread.Sleep(retryCount * 1000);
+                retryCount *= 2;
                 RefreshAzureToken();
-                retryCount--;
+                ExecuteControlCommand(command, retryCount);
+            }
+            catch (KustoRequestException) when (retryCount < RetryLimit)
+            {
+                Thread.Sleep(retryCount * 1000);
+                retryCount *= 2;
                 ExecuteControlCommand(command, retryCount);
             }
         }
