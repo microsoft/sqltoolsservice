01d0f03262d803b0d29d43175988fbb7aa69d772
Leila Lali <llali@microsoft.com>
2021-01-19 11:33:55 -0800

    Added service for model management for machine learning extension (#1138)
    * Added service for model management for ml extension

---
+++ src/Microsoft.SqlTools.ServiceLayer/ModelManagement/Contracts/DownloadModelRequestParams.cs
@@ -0,0 +1,38 @@
+﻿//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+using Microsoft.SqlTools.Hosting.Protocol.Contracts;
+
+namespace Microsoft.SqlTools.ServiceLayer.ModelManagement.Contracts
+{
+    public class DownloadModelRequestParams : ModelRequestBase
+    {
+        /// <summary>
+        /// Model id
+        /// </summary>
+        public int ModelId { get; set; }
+    }
+
+    /// <summary>
+    /// Response class for import model
+    /// </summary>
+    public class DownloadModelResponseParams : ModelResponseBase
+    {
+        /// <summary>
+        /// Downloaded file path
+        /// </summary>
+        public string FilePath { get; set; }
+    }
+
+    /// <summary>
+    /// Request class to delete a model
+    /// </summary>
+    public class DownloadModelRequest
+    {
+        public static readonly
+            RequestType<DownloadModelRequestParams, DownloadModelResponseParams> Type =
+                RequestType<DownloadModelRequestParams, DownloadModelResponseParams>.Create("models/download");
+    }
+}
