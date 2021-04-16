4e4bcecea5dd633d4637d9fbdd793e193be110e8
Karl Burtram <karlb@microsoft.com>
2021-04-16 13:07:03 -0700

    Move unused forked code to external directory

---
+++ external/Microsoft.SqlTools.DataProtocol.Contracts/Explorer/RefreshRequest.cs
@@ -0,0 +1,29 @@
+﻿//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+using Microsoft.SqlTools.Hosting.Contracts;
+
+namespace Microsoft.SqlTools.DataProtocol.Contracts.Explorer
+{
+    /// <summary>
+    /// Parameters to the <see cref="ExpandRequest"/>.
+    /// </summary>
+    public class RefreshParams: ExpandParams
+    {
+    }
+
+    /// <summary>
+    /// A request to expand a 
+    /// </summary>
+    public class RefreshRequest
+    {
+        /// <summary>
+        /// Returns children of a given node as a <see cref="NodeInfo"/> array.
+        /// </summary>
+        public static readonly
+            RequestType<RefreshParams, bool> Type =
+            RequestType<RefreshParams, bool>.Create("explorer/refresh");
+    }
+}
