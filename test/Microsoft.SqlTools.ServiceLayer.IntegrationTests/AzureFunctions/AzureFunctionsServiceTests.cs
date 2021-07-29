a8cbfda1dc4143ec666ea9d209e861b3d9d3ad63
Kim Santiago <kisantia@microsoft.com>
2021-07-29 15:48:14 -0700

    add another test

--- test/Microsoft.SqlTools.ServiceLayer.IntegrationTests/AzureFunctions/AzureFunctionsServiceTests.cs
+++ test/Microsoft.SqlTools.ServiceLayer.IntegrationTests/AzureFunctions/AzureFunctionsServiceTests.cs
@@ -12,7 +12,7 @@ namespace Microsoft.SqlTools.ServiceLayer.IntegrationTests.AzureFunctions
 {
     class AzureFunctionsServiceTests
     {
-        private string testAzureFunctionsFolder = Path.Combine("..", "..", "..", "AzureFunctions", "TestFiles");
+        private string testAzureFunctionsFolder = Path.Combine("..", "..", "..", "AzureFunctions", "AzureFunctionTestFiles");
 
         /// <summary>
         /// Verify input binding gets added
@@ -83,7 +83,7 @@ namespace Microsoft.SqlTools.ServiceLayer.IntegrationTests.AzureFunctions
         {
             // copy the original file because the input binding will be inserted into the file
             string originalFile = Path.Join(testAzureFunctionsFolder, "AzureFunctionsNoBindings.ts");
-            string testFile = Path.Join(Path.GetTempPath(), string.Format("InsertSqlInputBinding-{0}.ts", DateTime.Now.ToString("yyyy-dd-M--HH-mm-ss")));
+            string testFile = Path.Join(Path.GetTempPath(), string.Format("NoAzureFunctionForSqlBinding-{0}.ts", DateTime.Now.ToString("yyyy-dd-M--HH-mm-ss")));
             File.Copy(originalFile, testFile, true);
 
             InsertSqlBindingParams parameters = new InsertSqlBindingParams
@@ -100,6 +100,33 @@ namespace Microsoft.SqlTools.ServiceLayer.IntegrationTests.AzureFunctions
             Assert.False(result.Success);
             Assert.NotNull(result.ErrorMessage);
             Assert.True(result.ErrorMessage.Equals($"Couldn't find Azure function with FunctionName noExistingFunction in {testFile}"));
+        }
+
+        /// <summary>
+        /// Verify what happens when there's more than one Azure function with the specified name in the file
+        /// </summary>
+        [Test]
+        public async Task MoreThanOneAzureFunctionWithSpecifiedName()
+        {
+            // copy the original file because the input binding will be inserted into the file
+            string originalFile = Path.Join(testAzureFunctionsFolder, "AzureFunctionsMultipleSameFunction.ts");
+            string testFile = Path.Join(Path.GetTempPath(), string.Format("MoreThanOneAzureFunctionWithSpecifiedName-{0}.ts", DateTime.Now.ToString("yyyy-dd-M--HH-mm-ss")));
+            File.Copy(originalFile, testFile, true);
+
+            InsertSqlBindingParams parameters = new InsertSqlBindingParams
+            {
+                bindingType = BindingType.input,
+                filePath = testFile,
+                functionName = "GetArtists_get",
+                objectName = "[dbo].[table1]"
+            };
+
+            InsertSqlBindingOperation operation = new InsertSqlBindingOperation(parameters);
+            ResultStatus result = operation.AddBinding();
+
+            Assert.False(result.Success);
+            Assert.NotNull(result.ErrorMessage);
+            Assert.True(result.ErrorMessage.Equals($"More than one Azure function found with the FunctionName GetArtists_get in {testFile}"));
         }
     }
 }
