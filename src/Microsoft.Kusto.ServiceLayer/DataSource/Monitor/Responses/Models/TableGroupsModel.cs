3ed80640b1fa8a5479a1bf87a4573789cb20d0e9
Justin M <63619224+JustinMDotNet@users.noreply.github.com>
2021-08-05 09:05:58 -0700

    Changed logic around loading tables in MonitorDataSource. Added DisplayName to TableGroupsModel (#1225)

--- src/Microsoft.Kusto.ServiceLayer/DataSource/Monitor/Responses/Models/TableGroupsModel.cs
+++ src/Microsoft.Kusto.ServiceLayer/DataSource/Monitor/Responses/Models/TableGroupsModel.cs
@@ -2,6 +2,7 @@ namespace Microsoft.Kusto.ServiceLayer.DataSource.Monitor.Responses.Models
 {
     public class TableGroupsModel
     {
+        public string DisplayName { get; set; }
         public string Id { get; set; }
         public string Name { get; set; }
         public string Source { get; set; }
