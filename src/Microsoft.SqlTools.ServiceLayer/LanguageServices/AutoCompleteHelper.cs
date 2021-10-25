78398ffa32243c28a7e6a2bc7aa3ddba33b3f629
Aasim Khan <aasimkhan30@gmail.com>
2021-10-25 12:15:58 -0700

    Formatting file Removing dev setup removing unused imports

--- src/Microsoft.SqlTools.ServiceLayer/LanguageServices/AutoCompleteHelper.cs
+++ src/Microsoft.SqlTools.ServiceLayer/LanguageServices/AutoCompleteHelper.cs
@@ -6,7 +6,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using System.Threading;
 using Microsoft.SqlServer.Management.SqlParser.Binder;
 using Microsoft.SqlServer.Management.SqlParser.Intellisense;
