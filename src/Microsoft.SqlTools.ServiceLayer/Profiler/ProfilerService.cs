33585e2fb657bd08c028d12816519cdb1af8a162
Alex Ma <alma1@microsoft.com>
2021-10-26 15:57:19 -0700

    added startmonitoringsession

--- src/Microsoft.SqlTools.ServiceLayer/Profiler/ProfilerService.cs
+++ src/Microsoft.SqlTools.ServiceLayer/Profiler/ProfilerService.cs
@@ -189,7 +189,7 @@ namespace Microsoft.SqlTools.ServiceLayer.Profiler
                         // var eventStreamer = new XELiveEventStreamer(connectionString, parameters.SessionName);
                         // var readTask = eventStreamer.ReadEventStream(xEvents => HandleXEvent(xEvents, parameters.OwnerUri), threadCancellationToken.Token);
 
-                        monitor.StartMonitoringStream(parameters.OwnerUri, xeSession);
+                        monitor.StartMonitoringSession(parameters.OwnerUri, xeSession);
                         var result = new StartProfilingResult();
                         await requestContext.SendResult(result);
                     }
