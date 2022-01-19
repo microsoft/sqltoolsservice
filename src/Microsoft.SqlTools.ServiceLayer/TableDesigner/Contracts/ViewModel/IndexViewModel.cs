c9465a06ffa2fbeef08beb3a248890ddc5801475
Alan Ren <alanren@microsoft.com>
2022-01-18 17:15:22 -0800

    table designer - support indexes and write operations (#1363)
    * support indexes
    
    * column properties

---
+++ src/Microsoft.SqlTools.ServiceLayer/TableDesigner/Contracts/ViewModel/IndexViewModel.cs
@@ -0,0 +1,32 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+using Newtonsoft.Json;
+
+namespace Microsoft.SqlTools.ServiceLayer.TableDesigner.Contracts
+{
+    /// <summary>
+    /// The view model of index.
+    /// </summary>
+    public class IndexViewModel : ObjectViewModelBase
+    {
+        public CheckBoxProperties Enabled { get; set; } = new CheckBoxProperties();
+
+        public CheckBoxProperties IsClustered { get; set; } = new CheckBoxProperties();
+
+        public CheckBoxProperties IsUnique { get; set; } = new CheckBoxProperties();
+
+        public InputBoxProperties ColumnsDisplayValue { get; set; } = new InputBoxProperties();
+
+        public TableComponentProperties<IndexedColumnSpecification> Columns { get; set; } = new TableComponentProperties<IndexedColumnSpecification>();
+    }
+
+    public class IndexedColumnSpecification
+    {
+        public DropdownProperties Column { get; set; } = new DropdownProperties();
+
+        public CheckBoxProperties Ascending { get; set; } = new CheckBoxProperties();
+    }
+}
