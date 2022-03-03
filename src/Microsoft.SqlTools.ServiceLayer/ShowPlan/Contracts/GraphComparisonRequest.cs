b4e05b21594a6f69882be9b043c77216996c954d
Lewis Sanchez <lewissanchez@microsoft.com>
2022-03-03 13:42:11 -0800

    Adds missing copyright header and moves using statement below copyright

--- src/Microsoft.SqlTools.ServiceLayer/ShowPlan/Contracts/GraphComparisonRequest.cs
+++ src/Microsoft.SqlTools.ServiceLayer/ShowPlan/Contracts/GraphComparisonRequest.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.SqlTools.Hosting.Protocol.Contracts;
-using System.Collections.Generic;
+﻿//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+using Microsoft.SqlTools.Hosting.Protocol.Contracts;
 
 namespace Microsoft.SqlTools.ServiceLayer.ShowPlan.Contracts
 {
