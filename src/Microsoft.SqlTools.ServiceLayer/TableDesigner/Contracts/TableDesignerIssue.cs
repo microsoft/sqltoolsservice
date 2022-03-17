a9a29c88389d3419db51ee4eead01cb9536c2c75
Alan Ren <alanren@microsoft.com>
2022-03-17 10:16:42 -0700

    support issue severity (#1430)

---
+++ src/Microsoft.SqlTools.ServiceLayer/TableDesigner/Contracts/TableDesignerIssue.cs
@@ -0,0 +1,43 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Microsoft.SqlTools.ServiceLayer.TableDesigner.Contracts
+{
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum IssueSeverity
+    {
+        [EnumMember(Value = "error")]
+        Error,
+        [EnumMember(Value = "warning")]
+        Warning,
+        [EnumMember(Value = "informational")]
+        Informational,
+    }
+
+    /// <summary>
+    /// Table Designer Issue
+    /// </summary>
+    public class TableDesignerIssue
+    {
+        /// <summary>
+        /// The description.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// The property path associated with the message
+        /// </summary>
+        public object[] PropertyPath { get; set; }
+
+        /// <summary>
+        /// The severity of the message. Default value is Error.
+        /// </summary>
+        public IssueSeverity Severity { get; set; } = IssueSeverity.Error;
+    }
+}
