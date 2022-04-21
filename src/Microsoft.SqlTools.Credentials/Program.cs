0b8f3351b4431f4f52fedb935d2aaf73ec8dbf45
Charles Gagnon <chgagnon@microsoft.com>
2022-04-21 16:04:41 -0700

    Fix autoflush option not being used (#1470)
    * Fix autoflush option not being used
    
    * Remove extra comment

--- src/Microsoft.SqlTools.Credentials/Program.cs
+++ src/Microsoft.SqlTools.Credentials/Program.cs
@@ -38,9 +38,7 @@ namespace Microsoft.SqlTools.Credentials
                     logFilePath = Logger.GenerateLogFilePath("credentials");
                 }
 
-                Logger.AutoFlush = commandOptions.AutoFlushLog;
-
-                Logger.Initialize(tracingLevel: commandOptions.TracingLevel, logFilePath: logFilePath, traceSource: "credentials");
+                Logger.Initialize(tracingLevel: commandOptions.TracingLevel, logFilePath: logFilePath, traceSource: "credentials", commandOptions.AutoFlushLog);
 
                 // set up the host details and profile paths 
                 var hostDetails = new HostDetails(
