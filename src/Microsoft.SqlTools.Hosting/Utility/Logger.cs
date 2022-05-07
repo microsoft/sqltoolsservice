048988e5d323a6be1ec7257d4e815b73f4b4fe5d
Charles Gagnon <chgagnon@microsoft.com>
2022-05-06 22:47:42 -0700

    Add OE verbose logging (#1484)
    * Add OE verbose logging
    
    * verbose

--- src/Microsoft.SqlTools.Hosting/Utility/Logger.cs
+++ src/Microsoft.SqlTools.Hosting/Utility/Logger.cs
@@ -240,6 +240,36 @@ namespace Microsoft.SqlTools.Utility
         public static void Write(TraceEventType eventType, string logMessage) => Write(eventType, LogEvent.Default, logMessage);
 
         /// <summary>
+        /// Writes a message to the log file with the Verbose event level
+        /// </summary>
+        /// <param name="logMessage">The message text to be written.</param>
+        public static void Verbose(string logMessage) => Write(TraceEventType.Verbose, logMessage);
+
+        /// <summary>
+        /// Writes a message to the log file with the Information event level
+        /// </summary>
+        /// <param name="logMessage">The message text to be written.</param>
+        public static void Information(string logMessage) => Write(TraceEventType.Information, logMessage);
+
+        /// <summary>
+        /// Writes a message to the log file with the Warning event level
+        /// </summary>
+        /// <param name="logMessage">The message text to be written.</param>
+        public static void Warning(string logMessage) => Write(TraceEventType.Warning, logMessage);
+
+        /// <summary>
+        /// Writes a message to the log file with the Error event level
+        /// </summary>
+        /// <param name="logMessage">The message text to be written.</param>
+        public static void Error(string logMessage) => Write(TraceEventType.Error, logMessage);
+
+        /// <summary>
+        /// Writes a message to the log file with the Critical event level
+        /// </summary>
+        /// <param name="logMessage">The message text to be written.</param>
+        public static void Critical(string logMessage) => Write(TraceEventType.Critical, logMessage);
+
+        /// <summary>
         /// Writes a message to the log file with accompanying callstack.
         /// </summary>
         /// <param name="eventType">The level at which the message will be written.</param>
