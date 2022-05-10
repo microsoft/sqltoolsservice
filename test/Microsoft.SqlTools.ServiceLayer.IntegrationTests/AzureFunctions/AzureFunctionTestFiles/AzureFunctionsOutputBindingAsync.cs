3af029315ba3b7cc31c6008a5fa26311060e2208
Charles Gagnon <chgagnon@microsoft.com>
2022-05-10 11:09:45 -0700

    Fix bindings tests (#1488)

--- test/Microsoft.SqlTools.ServiceLayer.IntegrationTests/AzureFunctions/AzureFunctionTestFiles/AzureFunctionsOutputBindingAsync.cs
+++ test/Microsoft.SqlTools.ServiceLayer.IntegrationTests/AzureFunctions/AzureFunctionTestFiles/AzureFunctionsOutputBindingAsync.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
-using System.Collections.Generic;
 
 namespace Company.Namespace
 {
