d9f6f9f1f93b358979bacbc3b61c562942783bdb
Karl Burtram <karlb@microsoft.com>
2022-05-26 12:22:18 -0700

    Dacfx test fix

--- test/Microsoft.SqlTools.ServiceLayer.IntegrationTests/DacFx/DacFxServiceTests.cs
+++ test/Microsoft.SqlTools.ServiceLayer.IntegrationTests/DacFx/DacFxServiceTests.cs
@@ -781,6 +781,7 @@ FROM MissingEdgeHubInputStream'";
         public async Task ValidateGetDefaultPublishOptionsCallFromService()
         {
             DeploymentOptions expectedResults = DeploymentOptions.GetDefaultPublishOptions();
+            expectedResults.IgnoreTableOptions = new DeploymentOptionProperty<bool>(false);
 
             var dacfxRequestContext = new Mock<RequestContext<DacFxOptionsResult>>();
             dacfxRequestContext.Setup((RequestContext<DacFxOptionsResult> x) => x.SendResult(It.Is<DacFxOptionsResult>((result) => ValidateOptions(expectedResults, result.DeploymentOptions) == true))).Returns(Task.FromResult(new object()));
@@ -857,10 +858,11 @@ Streaming query statement contains a reference to missing output stream 'Missing
                 }
                 else
                 {
-                    // Verifying expected and actual deployment options properties are equal
+                    //Verifying expected and actual deployment options properties are equal
                     Assert.True((defaultP == null && actualP == null) 
                      || (defaultP == null && String.IsNullOrEmpty(actualP as string))
-                     || defaultP.Equals(actualP)
+                     // check the type of the properties, the descriptions will not match when overriding defaults
+                     || defaultP.GetType().Equals(actualP.GetType()) 
                     , $"Actual Property from Service is not equal to default property for {v.Name}, Actual property: {actualP} and Default property: {defaultP}");
 
                     // Verifying expected and actual deployment options property values are equal
