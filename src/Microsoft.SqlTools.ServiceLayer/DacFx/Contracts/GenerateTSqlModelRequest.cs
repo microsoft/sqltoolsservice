11dd29d8a0842a964eab302f788949e437ffc559
Barbara Valdez <34872381+barbaravaldez@users.noreply.github.com>
2022-07-29 12:04:16 -0700

    Add create model request (#1603)
    * add create model request

---
+++ src/Microsoft.SqlTools.ServiceLayer/DacFx/Contracts/GenerateTSqlModelRequest.cs
@@ -0,0 +1,39 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+using Microsoft.SqlTools.Hosting.Protocol.Contracts;
+using Microsoft.SqlTools.ServiceLayer.Utility;
+
+namespace Microsoft.SqlTools.ServiceLayer.DacFx.Contracts
+{
+    /// <summary>
+    /// Parameters to generate a SQL model
+    /// </summary>
+    public class GenerateTSqlModelParams
+    {
+        /// <summary>
+        /// URI of the project file this model is for
+        /// </summary>
+        public string ProjectUri { get; set; }
+
+        /// <summary>
+        /// The version of Sql Server to target
+        /// </summary>
+        public string ModelTargetVersion { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Sql script file paths.
+        /// </summary>
+        public string[] FilePaths { get; set; }
+    }
+
+    /// <summary>
+    /// Defines the generate sql model request
+    /// </summary>
+    class GenerateTSqlModelRequest
+    {
+        public static readonly RequestType<GenerateTSqlModelParams, ResultStatus> Type =
+            RequestType<GenerateTSqlModelParams, ResultStatus>.Create("dacFx/generateTSqlModel");
+    }
+}
