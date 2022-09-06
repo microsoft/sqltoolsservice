8f26a209500c773d4723b3234ef33908dcc1b359
Hai Cao <hacao@microsoft.com>
2022-09-06 13:32:43 -0700

    [Table Designer] support hash index and column store index (#1672)

---
+++ src/Microsoft.SqlTools.ServiceLayer/TableDesigner/Contracts/DesignerDataPropertyWithTabInfo.cs
@@ -0,0 +1,18 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+namespace Microsoft.SqlTools.ServiceLayer.TableDesigner.Contracts
+{
+    /// <summary>
+    /// Represents a component/property with tab info in the table designer
+    /// </summary>
+    public class DesignerDataPropertyWithTabInfo : DesignerDataPropertyInfo
+    {
+        /// <summary>
+        /// The tab of the property
+        /// </summary>
+        public string Tab { get; set; }
+    }
+}
