e72520f0498881f081ffa4190a2a0ac18b4b5c1b
Charles Gagnon <chgagnon@microsoft.com>
2022-09-16 15:04:54 -0700

    fix compile

--- test/Microsoft.SqlTools.ServiceLayer.Test.Common/TestServiceProvider.cs
+++ test/Microsoft.SqlTools.ServiceLayer.Test.Common/TestServiceProvider.cs
@@ -6,7 +6,6 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
