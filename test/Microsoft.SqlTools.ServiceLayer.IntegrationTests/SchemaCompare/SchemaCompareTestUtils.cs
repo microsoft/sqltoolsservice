6517de2b8dd12ae570195a162212d0aff564c4a8
Kim Santiago <kisantia@microsoft.com>
2022-11-03 11:36:58 -0700

    unique folder per test run

--- test/Microsoft.SqlTools.ServiceLayer.IntegrationTests/SchemaCompare/SchemaCompareTestUtils.cs
+++ test/Microsoft.SqlTools.ServiceLayer.IntegrationTests/SchemaCompare/SchemaCompareTestUtils.cs
@@ -103,7 +103,7 @@ namespace Microsoft.SqlTools.ServiceLayer.IntegrationTests.SchemaCompare
         /// <returns>Full path to the .sqlproj</returns>
         internal static string CreateSqlProj(string projectName)
         {
-            string folderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "SchemaCompareTest", projectName);
+            string folderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "SchemaCompareTest", TestContext.CurrentContext?.Test?.Name + DateTime.Now.Ticks.ToString(), projectName);
             Directory.CreateDirectory(folderPath);
             string sqlprojFilePath = Path.Combine(folderPath, projectName + ".sqlproj");
             File.Copy(Path.Combine(sqlProjectsFolder, "emptyTemplate.sqlproj"), sqlprojFilePath);
