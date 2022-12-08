52b294f4da8e97fe4e9d9bd1eb5edea343b3c16a
Hai Cao <hacao@microsoft.com>
2022-12-07 17:25:44 -0800

    add test case for parallel processing

--- test/Microsoft.SqlTools.ServiceLayer.UnitTests/Messaging/MessageDispatcherTests.cs
+++ test/Microsoft.SqlTools.ServiceLayer.UnitTests/Messaging/MessageDispatcherTests.cs
@@ -4,6 +4,8 @@
 //
 
 using System;
+using System.Diagnostics;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.SqlTools.Hosting.Protocol;
 using Microsoft.SqlTools.Hosting.Protocol.Channel;
@@ -84,5 +86,66 @@ namespace Microsoft.SqlTools.ServiceLayer.UnitTests.Messaging
                 Assert.True(handlerCalled);   
             }
         }
+
+        [Test]
+        public void ParallelMessageProcessingTest()
+        {
+            int numOfRequests = 11;
+            int msForEachRequest = 1000;
+            // Without parallel processing, this should take around numOfRequests * msForEachRequest ms to finish.
+            // With parallel process, this should take around 1 * msForEachRequest ms to finish.
+            // The diff should be greater than (numOfRequests - 1) * msForEachRequest ms.
+            Assert.IsTrue(GetTimeToHandleRequests(false, numOfRequests, msForEachRequest) - GetTimeToHandleRequests(true, numOfRequests, msForEachRequest) > msForEachRequest * (numOfRequests - 1));
+        }
+
+
+        /// <summary>
+        /// Gets the time to handle certain amount of requests in ms
+        /// </summary>
+        /// <param name="parallelMessageProcessing">Wheater to enable parallel processing</param>
+        /// <param name="numOfRequests">num of requests to handle</param>
+        /// <param name="msForEachRequest">rough time taken to finish each reqeust in ms</param>
+        /// <returns></returns>
+        private long GetTimeToHandleRequests(bool parallelMessageProcessing, int numOfRequests, int msForEachRequest)
+        {
+            RequestType<int, int> requestType = RequestType<int, int>.Create("test/requestType");
+            var mockChannel = new Mock<ChannelBase>();
+            SemaphoreSlim unfinishedRequestCount = new SemaphoreSlim(numOfRequests);
+            bool okayToEnd = false;
+            mockChannel.Setup(c => c.MessageReader.ReadMessage())
+                .Returns(Task.FromResult(Message.Request("1", "test/requestType", null)));
+            var dispatcher = new MessageDispatcher(mockChannel.Object);
+            dispatcher.ParallelMessageProcessing = parallelMessageProcessing;
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            var handler = async (int _, RequestContext<int> _) =>
+            {
+                Thread.Sleep(msForEachRequest);
+                unfinishedRequestCount.Wait();
+                if (unfinishedRequestCount.CurrentCount == 0)
+                {
+                    // cut off when we reach numOfRequests
+                    stopwatch.Stop();
+                    okayToEnd = true;
+                }
+                await Task.CompletedTask;
+            };
+
+            dispatcher.SetRequestHandler(requestType, handler, false, true);
+            dispatcher.Start();
+
+            while (true)
+            {
+                if (okayToEnd)
+                {
+                    // wait until we finish handling the required amount of requests
+                    break;
+                }
+                Thread.Sleep(1000);
+            }
+
+            return stopwatch.ElapsedMilliseconds;
+        }
+
+
     }
 }
