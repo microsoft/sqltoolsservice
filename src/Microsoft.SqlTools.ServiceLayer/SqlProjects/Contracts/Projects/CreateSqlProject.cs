2506a4df9c56041ba1bc497cd7f7867a4e7bfce9
Benjin Dubishar <benjin.dubishar@gmail.com>
2023-02-21 15:58:23 -0800

    Renaming NewSqlProject to CreateSqlProject to have verb-forward terminology (#1876)

--- src/Microsoft.SqlTools.ServiceLayer/SqlProjects/Contracts/Projects/CreateSqlProject.cs
+++ src/Microsoft.SqlTools.ServiceLayer/SqlProjects/Contracts/Projects/CreateSqlProject.cs
@@ -12,7 +12,7 @@ namespace Microsoft.SqlTools.ServiceLayer.SqlProjects.Contracts
     /// <summary>
     /// Parameters for creating a new SQL Project
     /// </summary>
-    public class NewSqlProjectParams : SqlProjectParams
+    public class CreateSqlProjectParams : SqlProjectParams
     {
         /// <summary>
         /// Type of SQL Project: SDK-style or Legacy
@@ -32,8 +32,8 @@ namespace Microsoft.SqlTools.ServiceLayer.SqlProjects.Contracts
         public string? BuildSdkVersion { get; set; }
     }
 
-    public class NewSqlProjectRequest
+    public class CreateSqlProjectRequest
     {
-        public static readonly RequestType<NewSqlProjectParams, ResultStatus> Type = RequestType<NewSqlProjectParams, ResultStatus>.Create("sqlProjects/newProject");
+        public static readonly RequestType<CreateSqlProjectParams, ResultStatus> Type = RequestType<CreateSqlProjectParams, ResultStatus>.Create("sqlProjects/createProject");
     }
 }
