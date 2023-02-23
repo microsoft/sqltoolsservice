fc4f3625fb564bc2df389f0cf73fef1254b7ab7b
Benjin Dubishar <benjin.dubishar@gmail.com>
2023-02-23 14:58:38 -0800

    Adding getters for project items

---
+++ src/Microsoft.SqlTools.ServiceLayer/SqlProjects/Contracts/SqlObjectScripts/GetSqlObjectScripts.cs
@@ -0,0 +1,28 @@
+﻿//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+#nullable disable
+
+using Microsoft.SqlTools.Hosting.Protocol.Contracts;
+using Microsoft.SqlTools.ServiceLayer.Utility;
+
+namespace Microsoft.SqlTools.ServiceLayer.SqlProjects.Contracts
+{
+    public class GetSqlObjectScriptsRequest
+    {
+        public static readonly RequestType<SqlProjectParams, GetScriptsResult> Type = RequestType<SqlProjectParams, GetScriptsResult>.Create("sqlProjects/getSqlObjectScripts");
+    }
+
+    /// <summary>
+    /// Result containing scripts in the project
+    /// </summary>
+    public class GetScriptsResult : ResultStatus
+    {
+        /// <summary>
+        /// Array of scripts contained in the project
+        /// </summary>
+        public string[] Scripts { get; set; }
+    }
+}
