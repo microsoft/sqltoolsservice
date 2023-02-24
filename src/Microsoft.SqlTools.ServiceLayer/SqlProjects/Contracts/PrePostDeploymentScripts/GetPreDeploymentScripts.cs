f7fd4788572bc2c457b80fa8ddcc8223e1bfdc0d
Benjin Dubishar <benjin.dubishar@gmail.com>
2023-02-23 16:25:01 -0800

    Adding getters for project items in SqlProject service (#1879)
    * Adding getters for project items
    
    * updated comment

---
+++ src/Microsoft.SqlTools.ServiceLayer/SqlProjects/Contracts/PrePostDeploymentScripts/GetPreDeploymentScripts.cs
@@ -0,0 +1,16 @@
+﻿//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+#nullable disable
+
+using Microsoft.SqlTools.Hosting.Protocol.Contracts;
+
+namespace Microsoft.SqlTools.ServiceLayer.SqlProjects.Contracts
+{
+    public class GetPreDeploymentScriptsRequest
+    {
+        public static readonly RequestType<SqlProjectParams, GetScriptsResult> Type = RequestType<SqlProjectParams, GetScriptsResult>.Create("sqlProjects/getPreDeploymentScripts");
+    }
+}
