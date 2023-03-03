c49f5c07e3155b1f8396e76fa86a81aed9827dc6
Hai Cao <hacao@microsoft.com>
2023-03-03 00:01:03 -0800

    fix language setting for sql auth login with azure

--- src/Microsoft.SqlTools.ServiceLayer/Security/LoginData.cs
+++ src/Microsoft.SqlTools.ServiceLayer/Security/LoginData.cs
@@ -2093,7 +2093,9 @@ INNER JOIN sys.sql_logins AS sql_logins
             this.SqlPassword = login.Password;
             this.OldPassword = login.OldPassword;
             this.LoginType = GetLoginType(login);
-            if (this.DefaultLanguage != null && 0 != String.Compare(login.DefaultLanguage, SR.DefaultLanguagePlaceholder, StringComparison.Ordinal))
+            if (this.DefaultLanguage != null 
+            && 0 != String.Compare(login.DefaultLanguage, SR.DefaultLanguagePlaceholder, StringComparison.Ordinal)
+            && (server.DatabaseEngineType == DatabaseEngineType.Standalone|| server.DatabaseEngineEdition == DatabaseEngineEdition.SqlManagedInstance))
             {
                 this.DefaultLanguage = login.DefaultLanguage.Split(" - ")[1];
             }
