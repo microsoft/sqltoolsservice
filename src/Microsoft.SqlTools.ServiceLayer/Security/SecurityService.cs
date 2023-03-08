d24a6233e9f92e6fa157fc65b3eceaa60206737d
Hai Cao <hacao@microsoft.com>
2023-03-08 15:22:09 -0800

    fix CanEditLockedOutState

--- src/Microsoft.SqlTools.ServiceLayer/Security/SecurityService.cs
+++ src/Microsoft.SqlTools.ServiceLayer/Security/SecurityService.cs
@@ -317,7 +317,7 @@ namespace Microsoft.SqlTools.ServiceLayer.Security
                 SupportWindowsAuthentication = prototype.WindowsAuthSupported,
                 SupportAADAuthentication = prototype.AADAuthSupported,
                 SupportSQLAuthentication = true, // SQL Auth support for login, not necessarily mean SQL Auth support for CONNECT etc.
-                CanEditLockedOutState = parameters.IsNewObject,
+                CanEditLockedOutState = !parameters.IsNewObject && prototype.IsLockedOut,
                 Databases = databases,
                 Languages = languages,
                 ServerRoles = prototype.ServerRoles.ServerRoleNames,
