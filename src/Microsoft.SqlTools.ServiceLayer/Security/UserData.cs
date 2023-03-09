d49f063aa121dbefe8e821cc6c52fd14e9284d08
Karl Burtram <karlb@microsoft.com>
2023-03-08 18:02:46 -0800

    Fix updating users in SQL DB (#1924) (#1927)

--- src/Microsoft.SqlTools.ServiceLayer/Security/UserData.cs
+++ src/Microsoft.SqlTools.ServiceLayer/Security/UserData.cs
@@ -12,6 +12,7 @@ using Microsoft.SqlServer.Management.Sdk.Sfc;
 using Microsoft.SqlTools.ServiceLayer.Management;
 using Microsoft.SqlTools.ServiceLayer.Security.Contracts;
 using Microsoft.SqlTools.ServiceLayer.Utility;
+using Microsoft.SqlServer.Management.Common;
 
 namespace Microsoft.SqlTools.ServiceLayer.Security
 {
@@ -237,8 +238,13 @@ namespace Microsoft.SqlTools.ServiceLayer.Security
             if (SqlMgmtUtils.IsSql11OrLater(context.Server.ServerVersion))
             {
                 this.authenticationType = existingUser.AuthenticationType;
-                this.defaultLanguageAlias = LanguageUtils.GetLanguageAliasFromName(existingUser.Parent.Parent,
-                                                                existingUser.DefaultLanguage.Name);
+
+                if (context.Server.ServerType != DatabaseEngineType.SqlAzureDatabase)
+                {
+                    this.defaultLanguageAlias = LanguageUtils.GetLanguageAliasFromName(
+                                                                    existingUser.Parent.Parent,
+                                                                    existingUser.DefaultLanguage.Name);
+                }
             }
         }
 
@@ -793,7 +799,8 @@ namespace Microsoft.SqlTools.ServiceLayer.Security
             get
             {
                 //Default Language was not supported before Denali.
-                return SqlMgmtUtils.IsSql11OrLater(this.context.Server.ConnectionContext.ServerVersion);
+                return SqlMgmtUtils.IsSql11OrLater(this.context.Server.ConnectionContext.ServerVersion)
+                    && this.context.Server.ServerType != DatabaseEngineType.SqlAzureDatabase;
             }
         }
 
