a37093a773ab92d646768005be1b834a217c66a8
Leila Lali <llali@microsoft.com>
2023-04-11 14:22:05 -0700

    Enabling DacFx telemetry (#1993)
    * Enabling DacFx telemetry

---
+++ src/Microsoft.SqlTools.ServiceLayer/SqlContext/TelemetryLevel.cs
@@ -0,0 +1,15 @@
+﻿//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+namespace Microsoft.SqlTools.ServiceLayer.SqlContext
+{
+    public enum TelemetryLevel
+    {
+        All,
+        Error,
+        Crash,
+        Off
+    }
+}
