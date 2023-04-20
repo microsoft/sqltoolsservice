35274cd323b04322bc957d969e764e9bc11540f6
Hai Cao <hacao@microsoft.com>
2023-04-20 16:05:39 -0700

    contracts

---
+++ src/Microsoft.SqlTools.ServiceLayer/ObjectManagement/ObjectTypes/DatabaseRole/DatabaseRoleViewInfo.cs
@@ -0,0 +1,15 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+namespace Microsoft.SqlTools.ServiceLayer.ObjectManagement
+{
+    /// <summary>
+    /// a class for storing various database role properties
+    /// </summary>
+    public class DatabaseRoleViewInfo
+    {
+        public DatabaseRoleInfo? DatabaseRole { get; set; }
+    }
+}
