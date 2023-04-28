f66a203c017c179f080d646c848373710df5c28d
Cheena Malhotra <13396919+cheenamalhotra@users.noreply.github.com>
2023-04-28 13:39:33 -0700

    Fix application names to respect connection's appname property (#2034)

--- src/Microsoft.SqlTools.ServiceLayer/ObjectExplorer/ObjectExplorerService.cs
+++ src/Microsoft.SqlTools.ServiceLayer/ObjectExplorer/ObjectExplorerService.cs
@@ -399,7 +399,7 @@ namespace Microsoft.SqlTools.ServiceLayer.ObjectExplorer
                     {
                         var builder = ConnectionService.CreateConnectionStringBuilder(session.ConnectionInfo.ConnectionDetails);
                         builder.InitialCatalog = node.NodeValue;
-                        builder.ApplicationName = TableDesignerService.TableDesignerApplicationName;
+                        builder.ApplicationName = ConnectionService.GetApplicationNameWithFeature(builder.ApplicationName, TableDesignerService.TableDesignerApplicationNameSuffix);
                         // Set Access Token only when authentication mode is not specified.
                         var azureToken = builder.Authentication == SqlAuthenticationMethod.NotSpecified
                             ? session.ConnectionInfo.ConnectionDetails.AzureAccountToken : null;
