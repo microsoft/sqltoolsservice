961e8e38a54d62550223eb7d647871c1c7bc6d17
Cory Rivera <corivera@microsoft.com>
2023-05-04 14:22:07 -0700

    Clean up imports

--- src/Microsoft.SqlTools.ServiceLayer/ObjectManagement/ObjectTypes/Database/DatabaseHandler.cs
+++ src/Microsoft.SqlTools.ServiceLayer/ObjectManagement/ObjectTypes/Database/DatabaseHandler.cs
@@ -3,22 +3,10 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 //
 
-#nullable disable
 using System;
 using System.Threading.Tasks;
-using Microsoft.SqlServer.Management.Common;
-using Microsoft.SqlServer.Management.Sdk.Sfc;
-using Microsoft.SqlServer.Management.Smo;
 using Microsoft.SqlTools.ServiceLayer.Connection;
-using Microsoft.SqlTools.ServiceLayer.Management;
 using Microsoft.SqlTools.ServiceLayer.ObjectManagement.Contracts;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using Microsoft.SqlTools.ServiceLayer.Admin;
-using static Microsoft.SqlTools.ServiceLayer.Admin.AzureSqlDbHelper;
-using System.Globalization;
-using System.Xml;
 
 namespace Microsoft.SqlTools.ServiceLayer.ObjectManagement
 {
