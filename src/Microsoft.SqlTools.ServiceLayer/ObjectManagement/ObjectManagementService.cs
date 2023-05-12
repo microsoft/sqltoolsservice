f2421bce831b2fb921fe61d1887fb2ac2395c03d
Hai Cao <hacao@microsoft.com>
2023-05-11 19:08:06 -0700

    Fix schema handling in search result of User Management (#2057)
    * fix schema filed in search result
    
    * fix search for securable

--- src/Microsoft.SqlTools.ServiceLayer/ObjectManagement/ObjectManagementService.cs
+++ src/Microsoft.SqlTools.ServiceLayer/ObjectManagement/ObjectManagementService.cs
@@ -173,7 +173,8 @@ namespace Microsoft.SqlTools.ServiceLayer.ObjectManagement
                     res.Add(new SearchResultItem
                     {
                         Name = obj.Name,
-                        Type = type
+                        Type = type,
+                        Schema = obj.Schema
                     });
                 }
             }
