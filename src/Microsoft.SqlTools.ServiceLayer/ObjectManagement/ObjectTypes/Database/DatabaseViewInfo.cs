d5cfc52ca75c2fda279cb374e84c62f0b0ea61ea
Cory Rivera <corivera@microsoft.com>
2023-05-31 12:55:12 -0700

    Add object management handler for creating a database (#2071)

---
+++ src/Microsoft.SqlTools.ServiceLayer/ObjectManagement/ObjectTypes/Database/DatabaseViewInfo.cs
@@ -0,0 +1,18 @@
+
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+#nullable disable
+
+namespace Microsoft.SqlTools.ServiceLayer.ObjectManagement
+{
+    public class DatabaseViewInfo : SqlObjectViewInfo
+    {
+        public string[] LoginNames { get; set; }
+        public string[] CollationNames { get; set; }
+        public string[] CompatibilityLevels { get; set; }
+        public string[] ContainmentTypes { get; set; }
+        public string[] RecoveryModels { get; set; }
+    }
+}
