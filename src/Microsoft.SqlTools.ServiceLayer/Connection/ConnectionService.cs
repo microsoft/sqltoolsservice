a9af047628ff2b8edcb28d97eb1c0e5175028a7b
Alex Ma <alma1@microsoft.com>
2023-06-12 16:08:57 -0700

    added handling for multiple connections with same basic info

--- src/Microsoft.SqlTools.ServiceLayer/Connection/ConnectionService.cs
+++ src/Microsoft.SqlTools.ServiceLayer/Connection/ConnectionService.cs
@@ -1044,7 +1044,19 @@ namespace Microsoft.SqlTools.ServiceLayer.Connection
             {
                 try
                 {
-                    connection.Close();
+                    int totalCount = 0;
+                    foreach (KeyValuePair<string, ConnectionInfo> entry in OwnerToConnectionMap)
+                    {
+                        foreach (DbConnection value in entry.Value.AllConnections) {
+                            if(value.ConnectionString == connection.ConnectionString) {
+                                totalCount++;
+                            }
+                        }
+                    }
+
+                    if(totalCount == 1) {
+                        connection.Close();
+                    }
                 }
                 catch (Exception)
                 {
