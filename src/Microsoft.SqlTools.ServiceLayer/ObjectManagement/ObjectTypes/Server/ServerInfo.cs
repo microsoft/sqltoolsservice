4d6ecb80a2a978f679a678af5891790ad47fb6ff
Barbara Valdez Mireles <bavaldez@microsoft.com>
2023-06-22 11:49:07 -0700

    remove unnecessary directive

--- src/Microsoft.SqlTools.ServiceLayer/ObjectManagement/ObjectTypes/Server/ServerInfo.cs
+++ src/Microsoft.SqlTools.ServiceLayer/ObjectManagement/ObjectTypes/Server/ServerInfo.cs
@@ -3,8 +3,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 //
 
-using Microsoft.SqlTools.ServiceLayer.ServerConfigurations.Contracts;
-
 namespace Microsoft.SqlTools.ServiceLayer.ObjectManagement
 {
     /// <summary>
