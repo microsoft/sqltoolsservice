51d8d821e6ce9a4ec67199b368f7af68aff115f4
Benjin Dubishar <benjin.dubishar@gmail.com>
2023-07-18 10:24:49 -0700

    Checkpoint

---
+++ src/Microsoft.SqlTools.ServiceLayer/QueryStore/QueryStoreService.cs
@@ -0,0 +1,58 @@
+﻿//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Dac.Projects;
+using Microsoft.SqlTools.Hosting.Protocol;
+using Microsoft.SqlTools.ServiceLayer.Hosting;
+using Microsoft.SqlTools.ServiceLayer.QueryStore.Contracts;
+using Microsoft.SqlTools.ServiceLayer.Utility;
+
+namespace Microsoft.SqlTools.ServiceLayer.SqlProjects
+{
+    /// <summary>
+    /// Main class for SqlProjects service
+    /// </summary>
+    public sealed class QueryStoreService : BaseService
+    {
+        private static readonly Lazy<QueryStoreService> instance = new Lazy<QueryStoreService>(() => new QueryStoreService());
+
+        /// <summary>
+        /// Gets the singleton instance object
+        /// </summary>
+        public static QueryStoreService Instance => instance.Value;
+
+        private Lazy<ConcurrentDictionary<string, SqlProject>> projects = new Lazy<ConcurrentDictionary<string, SqlProject>>(() => new ConcurrentDictionary<string, SqlProject>(StringComparer.OrdinalIgnoreCase));
+
+        /// <summary>
+        /// <see cref="ConcurrentDictionary{String, TSqlModel}"/> that maps Project URI to Project
+        /// </summary>
+        public ConcurrentDictionary<string, SqlProject> Projects => projects.Value;
+
+        /// <summary>
+        /// Initializes the service instance
+        /// </summary>
+        /// <param name="serviceHost"></param>
+        public void InitializeService(ServiceHost serviceHost)
+        {
+            serviceHost.SetRequestHandler(OpenSqlProjectRequest.Type, HandleOpenSqlProjectRequest, isParallelProcessingSupported: true);
+        }
+
+        #region Handlers
+
+        #region Project-level functions
+
+        internal async Task HandleOpenSqlProjectRequest(SqlProjectParams requestParams, RequestContext<ResultStatus> requestContext)
+        {
+            await RunWithErrorHandling(() => GetProject(requestParams.ProjectUri), requestContext);
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
