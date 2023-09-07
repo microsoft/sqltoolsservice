703691e8970f3364ed4d9cfc412983e588dede5a
Cheena Malhotra <13396919+cheenamalhotra@users.noreply.github.com>
2023-09-07 12:03:56 -0700

    Support clearing pooled connections (#2214)

---
+++ src/Microsoft.SqlTools.ServiceLayer/Connection/Contracts/ClearPooledConnectionsRequest.cs
@@ -0,0 +1,18 @@
+﻿//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+using Microsoft.SqlTools.Hosting.Protocol.Contracts;
+
+namespace Microsoft.SqlTools.ServiceLayer.Connection.Contracts
+{
+    /// <summary>
+    /// Clear Pooled connectins request
+    /// </summary>
+    public class ClearPooledConnectionsRequest
+    {
+        public static readonly
+            RequestType<object, bool> Type = RequestType<object, bool>.Create("connection/clearpooledconnections");
+    }
+}
