511d451549e1dc26185e81186d484bdf10cf1a21
Barbara Valdez Mireles <bavaldez@microsoft.com>
2023-09-10 23:58:47 -0700

    get max memory values directly from server and fix conditions

--- src/Microsoft.SqlTools.ServiceLayer/ObjectManagement/ObjectTypes/Server/ServerPrototype.cs
+++ src/Microsoft.SqlTools.ServiceLayer/ObjectManagement/ObjectTypes/Server/ServerPrototype.cs
@@ -33,9 +33,6 @@ namespace Microsoft.SqlTools.ServiceLayer.ObjectManagement
 
         private ServerPrototypeData currentState;
         private ServerPrototypeData originalState;
-
-        private ConfigProperty serverMinMemoryProperty;
-        private ConfigProperty serverMaxMemoryProperty;
         #endregion
 
         #region Trace support
@@ -615,8 +612,6 @@ namespace Microsoft.SqlTools.ServiceLayer.ObjectManagement
             this.configService = new ServerConfigService();
             this.currentState = new ServerPrototypeData(context, context.Server, this.configService);
             this.originalState = (ServerPrototypeData)this.currentState.Clone();
-            this.serverMaxMemoryProperty = this.configService.GetServerSmoConfig(context.Server, this.configService.MaxServerMemoryPropertyNumber);
-            this.serverMinMemoryProperty = this.configService.GetServerSmoConfig(context.Server, this.configService.MinServerMemoryPropertyNumber);
         }
 
         #endregion
@@ -632,42 +627,13 @@ namespace Microsoft.SqlTools.ServiceLayer.ObjectManagement
             if (this.dataContainer.Server != null)
             {
                 Server server = this.dataContainer.Server;
-                bool alterServerConfig = false;
-                bool sendCPUAffinityBeforeIO = false;
-                bool sendIOAffinityBeforeCPU = false;
-                bool sentCpuAffinity = false;
-
-                sendCPUAffinityBeforeIO = this.CheckCPUAffinityBeforeIO(server);
-                sendIOAffinityBeforeCPU = this.CheckIOAffinityBeforeCPU(server);
-                alterServerConfig = this.CheckIOAffinityTsqlGenerated(server);
-                if (!sendIOAffinityBeforeCPU)
-                {
-                    SendDataForKJ(server);
-                    sentCpuAffinity = true;
-                }
-
-                if (alterServerConfig)
-                {
-                    try
-                    {
-                        server.Configuration.Alter((sendCPUAffinityBeforeIO && sendIOAffinityBeforeCPU));
-                    }
-                    finally
-                    {
-                        server.Configuration.Refresh();
-                    }
-                }
-                if (!sentCpuAffinity)
-                {
-                    SendDataForKJ(server);
-                }
-                this.currentState.AffinityManagerProcessorMask.Clear();
-                this.currentState.AffinityManagerIOMask.Clear();
 
                 if (UpdateMemoryValues(this.dataContainer.Server))
                 {
                     server.Configuration.Alter(true);
                 }
+
+                UpdateProcessorsValues(this.dataContainer.Server);
 
                 if (UpdateSecurityValues(this.dataContainer.Server))
                 {
@@ -702,17 +668,53 @@ namespace Microsoft.SqlTools.ServiceLayer.ObjectManagement
             if (this.currentState.MinMemory.Value != this.originalState.MinMemory.Value)
             {
                 changesMade = true;
-                ConfigProperty serverConfig = this.configService.GetServerSmoConfig(server, this.configService.MinServerMemoryPropertyNumber);
-                serverConfig.ConfigValue = this.currentState.MinMemory.Value;
+                server.Configuration.MinServerMemory.ConfigValue = this.currentState.MinMemory.Value;
             }
 
             if (this.currentState.MaxMemory.Value != this.originalState.MaxMemory.Value)
             {
                 changesMade = true;
-                ConfigProperty serverConfig = this.configService.GetServerSmoConfig(server, this.configService.MaxServerMemoryPropertyNumber);
-                serverConfig.ConfigValue = this.currentState.MaxMemory.Value;
+                server.Configuration.MaxServerMemory.ConfigValue = this.currentState.MaxMemory.Value;
             }
             return changesMade;
+        }
+
+        public void UpdateProcessorsValues(Server server)
+        {
+            bool alterServerConfig = false;
+            bool sendCPUAffinityBeforeIO = false;
+            bool sendIOAffinityBeforeCPU = false;
+            bool sentCpuAffinity = false;
+            if(this.currentState.AutoProcessorAffinityIOMaskForAll != this.originalState.AutoProcessorAffinityIOMaskForAll ||
+                this.currentState.AutoProcessorAffinityMaskForAll != this.originalState.AutoProcessorAffinityMaskForAll)
+            {
+                sendCPUAffinityBeforeIO = this.CheckCPUAffinityBeforeIO(server);
+                sendIOAffinityBeforeCPU = this.CheckIOAffinityBeforeCPU(server);
+                alterServerConfig = this.CheckIOAffinityTsqlGenerated(server);
+                if (!sendIOAffinityBeforeCPU)
+                {
+                    SendDataForKJ(server);
+                    sentCpuAffinity = true;
+                }
+
+                if (alterServerConfig)
+                {
+                    try
+                    {
+                        server.Configuration.Alter((sendCPUAffinityBeforeIO && sendIOAffinityBeforeCPU));
+                    }
+                    finally
+                    {
+                        server.Configuration.Refresh();
+                    }
+                }
+                if (!sentCpuAffinity)
+                {
+                    SendDataForKJ(server);
+                }
+                this.currentState.AffinityManagerProcessorMask.Clear();
+                this.currentState.AffinityManagerIOMask.Clear();
+            }
         }
 
         public bool UpdateSecurityValues(Server server)
@@ -796,13 +798,13 @@ namespace Microsoft.SqlTools.ServiceLayer.ObjectManagement
                 alterServer = true;
             }
 
-            if (this.currentState.BlockedProcThreshold != this.originalState.BlockedProcThreshold)
+            if (this.currentState.BlockedProcThreshold.Value != this.originalState.BlockedProcThreshold.Value)
             {
                 server.Configuration.BlockedProcessThreshold.ConfigValue = this.currentState.BlockedProcThreshold.Value;
                 alterServer = true;
             }
 
-            if (this.currentState.CursorThreshold != this.originalState.CursorThreshold)
+            if (this.currentState.CursorThreshold.Value != this.originalState.CursorThreshold.Value)
             {
                 server.Configuration.CursorThreshold.ConfigValue = this.currentState.CursorThreshold.Value;
                 alterServer = true;
@@ -844,25 +846,25 @@ namespace Microsoft.SqlTools.ServiceLayer.ObjectManagement
                 alterServer = true;
             }
 
-            if (this.currentState.CostThresholdParallelism != this.originalState.CostThresholdParallelism)
+            if (this.currentState.CostThresholdParallelism.Value != this.originalState.CostThresholdParallelism.Value)
             {
                 server.Configuration.CostThresholdForParallelism.ConfigValue = this.currentState.CostThresholdParallelism.Value;
                 alterServer = true;
             }
 
-            if (this.currentState.Locks != this.originalState.Locks)
+            if (this.currentState.Locks.Value != this.originalState.Locks.Value)
             {
                 server.Configuration.Locks.ConfigValue = this.currentState.Locks.Value;
                 alterServer = true;
             }
 
-            if (this.currentState.MaxDegreeParallelism != this.originalState.MaxDegreeParallelism)
+            if (this.currentState.MaxDegreeParallelism.Value != this.originalState.MaxDegreeParallelism.Value)
             {
                 server.Configuration.MaxDegreeOfParallelism.ConfigValue = this.currentState.MaxDegreeParallelism.Value;
                 alterServer = true;
             }
 
-            if (this.currentState.QueryWait != this.originalState.QueryWait)
+            if (this.currentState.QueryWait.Value != this.originalState.QueryWait.Value)
             {
                 server.Configuration.QueryWait.ConfigValue = this.currentState.QueryWait.Value;
                 alterServer = true;
@@ -1134,9 +1136,6 @@ namespace Microsoft.SqlTools.ServiceLayer.ObjectManagement
             private bool isYukonOrLater = false;
             private bool isSqlServer64Bit;
             private bool isIOAffinitySupported = false;
-
-            ConfigProperty serverMaxMemoryProperty;
-            ConfigProperty serverMinMemoryProperty;
             #endregion
 
             #region Properties
@@ -2187,8 +2186,6 @@ namespace Microsoft.SqlTools.ServiceLayer.ObjectManagement
                 this.isSqlServer64Bit = (this.server.Edition.Contains("(64 - bit)"));
                 this.affinityManagerIOMask = new AffinityManager();
                 this.affinityManagerProcessorMask = new AffinityManager();
-                this.serverMaxMemoryProperty = this.configService.GetServerSmoConfig(server, this.configService.MaxServerMemoryPropertyNumber);
-                this.serverMinMemoryProperty = this.configService.GetServerSmoConfig(server, this.configService.MinServerMemoryPropertyNumber);
                 this.minMemory = new NumericServerProperty();
                 this.maxMemory = new NumericServerProperty();
                 this.blockedProcThreshold = new NumericServerProperty();
@@ -2292,13 +2289,13 @@ namespace Microsoft.SqlTools.ServiceLayer.ObjectManagement
             }
             private void LoadMemoryProperties()
             {
-                this.maxMemory.Value = serverMaxMemoryProperty.ConfigValue;
-                this.maxMemory.MaximumValue = serverMaxMemoryProperty.Maximum;
-                this.maxMemory.MinimumValue = serverMaxMemoryProperty.Minimum;
+                this.maxMemory.Value = server.Configuration.MaxServerMemory.ConfigValue;
+                this.maxMemory.MaximumValue = server.Configuration.MaxServerMemory.Maximum;
+                this.maxMemory.MinimumValue = server.Configuration.MaxServerMemory.Minimum;
 
-                this.minMemory.Value = serverMinMemoryProperty.ConfigValue;
-                this.minMemory.MaximumValue = serverMinMemoryProperty.Maximum;
-                this.minMemory.MinimumValue = serverMinMemoryProperty.Minimum;
+                this.minMemory.Value = server.Configuration.MinServerMemory.ConfigValue;
+                this.minMemory.MaximumValue = server.Configuration.MinServerMemory.Maximum;
+                this.minMemory.MinimumValue = server.Configuration.MinServerMemory.Minimum;
             }
 
             private void LoadProcessorsProperties()
