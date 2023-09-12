70bcac1ef52c1049580cd66db4fd6c03670f0087
Sai Avishkar Sreerama <74571829+ssreerama@users.noreply.github.com>
2023-09-12 14:13:54 -0500

    queryStore WaitStatistics options should be a dropdown #24372 (#2228) (#2235)

--- src/Microsoft.SqlTools.SqlCore/Utility/CommonConstants.cs
+++ src/Microsoft.SqlTools.SqlCore/Utility/CommonConstants.cs
@@ -18,8 +18,8 @@ namespace Microsoft.SqlTools.SqlCore.Utility
         public const string DefaultBatchSeperator = "GO";
 
         // Database scoped configurations property values
-        public const string DatabaseScopedConfigurations_Value_On = "ON";
-        public const string DatabaseScopedConfigurations_Value_Off = "OFF";
+        public const string PropertiesDropdown_Value_On = "ON";
+        public const string PropertiesDropdown_Value_Off = "OFF";
         public const string DatabaseScopedConfigurations_Value_Primary = "PRIMARY";
         public const string DatabaseScopedConfigurations_Value_When_supported = "WHEN_SUPPORTED";
         public const string DatabaseScopedConfigurations_Value_Fail_Unsupported = "FAIL_UNSUPPORTED";
