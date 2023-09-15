18d3cc07db561e21b9b3c1b9fca04f754776f1bd
Steven Marturano <smartura@microsoft.com>
2023-09-14 21:07:02 -0400

    Updated TDE NuGet and contracts

---
+++ src/Microsoft.SqlTools.Migration/Contracts/TdeValidationTitlesRequest.cs
@@ -0,0 +1,19 @@
+﻿//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+using Microsoft.SqlTools.Hosting.Protocol.Contracts;
+
+namespace Microsoft.SqlTools.Migration.Contracts
+{
+    public class TdeValidationTitlesParams
+    {
+    }
+
+    public class TdeValidationTitlesRequest
+    {
+        public static readonly RequestType<TdeValidationTitlesParams, string[]> Type =
+            RequestType<TdeValidationTitlesParams, string[]>.Create("migration/tdevalidationtitles");
+    }
+}
