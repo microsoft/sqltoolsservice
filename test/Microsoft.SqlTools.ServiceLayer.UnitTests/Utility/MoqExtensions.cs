373c42469ab568db30f918a3d0de011c9ff62440
Cheena Malhotra <cmalhotra@microsoft.com>
2023-09-20 10:52:54 -0700

    Version bumps

--- test/Microsoft.SqlTools.ServiceLayer.UnitTests/Utility/MoqExtensions.cs
+++ test/Microsoft.SqlTools.ServiceLayer.UnitTests/Utility/MoqExtensions.cs
@@ -17,12 +17,6 @@ namespace Microsoft.SqlTools.ServiceLayer.UnitTests.Utility
 
         public delegate void OutAction<in T1, TOut>(T1 arg1, out TOut outVal);
 
-        public static IReturnsThrows<TMock, TReturn> OutCallback<TMock, TReturn, TOut>(
-            this ICallback<TMock, TReturn> mock, OutAction<TOut> action) where TMock : class
-        {
-            return OutCallbackInternal(mock, action);
-        }
-
         public static IReturnsThrows<TMock, TReturn> OutCallback<TMock, TReturn, T1, TOut>(
             this ICallback<TMock, TReturn> mock, OutAction<T1, TOut> action) where TMock : class
         {
