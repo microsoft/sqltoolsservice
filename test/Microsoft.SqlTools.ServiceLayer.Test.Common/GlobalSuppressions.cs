353b159512720f94954f9eab26e9c29d63717654
Cory Rivera <corivera@microsoft.com>
2024-03-18 19:24:37 -0700

    Add more suppression rules

---
+++ test/Microsoft.SqlTools.ServiceLayer.Test.Common/GlobalSuppressions.cs
@@ -0,0 +1,12 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0290:Use primary constructor", Scope = "module")]
+[assembly: SuppressMessage("Style", "IDE0301:Simplify collection initialization", Scope = "module")]
+[assembly: SuppressMessage("Style", "IDE0305:Simplify collection initialization", Scope = "module")]
+[assembly: SuppressMessage("Performance", "CA1859:Use concrete types when possible for improved performance", Scope = "module")]
+[assembly: SuppressMessage("Performance", "CA1864:Prefer the 'IDictionary.TryAdd(TKey, TValue)' method", Scope = "module")]
