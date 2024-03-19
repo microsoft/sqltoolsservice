7470b071ab8af0f5ed6b10171f576a8a901e88a2
Cory Rivera <corivera@microsoft.com>
2024-03-18 18:51:28 -0700

    Upgrade Kusto and SQL Tools Service projects to net8.0

---
+++ src/Microsoft.SqlTools.ManagedBatchParser/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Performance", "CA1859:Use concrete types when possible for improved performance", Scope = "module")]
+[assembly: SuppressMessage("Performance", "CA1860:Avoid using 'Enumerable.Any()' extension method", Scope = "module")]
---
+++ src/Microsoft.SqlTools.ServiceLayer/GlobalSuppressions.cs
@@ -0,0 +1,24 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0290:Use primary constructor", Scope = "module")]
+[assembly: SuppressMessage("Style", "IDE0300:Simplify collection initialization", Scope = "module")]
+[assembly: SuppressMessage("Style", "IDE0301:Simplify collection initialization", Scope = "module")]
+[assembly: SuppressMessage("Style", "IDE0305:Simplify collection initialization", Scope = "module")]
+[assembly: SuppressMessage("Maintainability", "CA1510:Use ArgumentNullException throw helper", Scope = "module")]
+[assembly: SuppressMessage("Maintainability", "CA1513:Use ObjectDisposedException throw helper", Scope = "module")]
+[assembly: SuppressMessage("Performance", "CA1854:Prefer the 'IDictionary.TryGetValue(TKey, out TValue)' method", Scope = "module")]
+[assembly: SuppressMessage("Performance", "CA1859:Use concrete types when possible for improved performance", Scope = "module")]
+[assembly: SuppressMessage("Performance", "CA1860:Avoid using 'Enumerable.Any()' extension method", Scope = "module")]
+[assembly: SuppressMessage("Performance", "CA1861:Avoid constant arrays as arguments", Scope = "module")]
+[assembly: SuppressMessage("Performance", "CA1862:Use the 'StringComparison' method overloads to perform case-insensitive string comparisons", Scope = "module")]
+[assembly: SuppressMessage("Performance", "CA1863:Use 'CompositeFormat'", Scope = "module")]
+[assembly: SuppressMessage("Performance", "CA1864:Prefer the 'IDictionary.TryAdd(TKey, TValue)' method", Scope = "module")]
+[assembly: SuppressMessage("Performance", "CA1865:Use char overload", Scope = "module")]
+[assembly: SuppressMessage("Performance", "CA1866:Use char overload", Scope = "module")]
+[assembly: SuppressMessage("Performance", "CA1868:Unnecessary call to 'Contains(item)'", Scope = "module")]
+[assembly: SuppressMessage("Usage", "CA2250:Use 'ThrowIfCancellationRequested'", Scope = "module")]
