19c8d599d42f9f7ac9f03cdc66fd2c46ce256ca3
Karl Burtram <karlb@microsoft.com>
2024-07-16 11:21:19 -0700

    Add a test for the SQL document rename scenario

--- test/Microsoft.SqlTools.ServiceLayer.IntegrationTests/QueryExecution/QueryExecutionServiceTests.cs
+++ test/Microsoft.SqlTools.ServiceLayer.IntegrationTests/QueryExecution/QueryExecutionServiceTests.cs
@@ -3,8 +3,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 //
 
-#nullable disable
-
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -24,9 +22,11 @@ namespace Microsoft.SqlTools.ServiceLayer.IntegrationTests.QueryExecution
     /// </summary>
     public class QueryExecutionServiceTests
     {
-        
-
-
+        /// <summary>
+        /// Test that session-level query execution options are persisted across executions on an active 
+        /// connection.  This runs a query with the default options, changes the options and runs query
+        /// in a batch, then finally runs a query to confirm the options changes are still applied.
+        /// </summary>
         [Test]
         public async Task QueryExecutionSessionOptionsPersistAcrossQueries()
         {
@@ -65,11 +65,64 @@ namespace Microsoft.SqlTools.ServiceLayer.IntegrationTests.QueryExecution
             }
         }
 
+        /// <summary>
+        /// Tests that verifies that the session-level query execution options persist after changing
+        /// the connection URI.  The connection URI changes when a script document is renamed.
+        /// </summary>
+        [Test]
+        public async Task QueryExecutionSessionOptionsPersistsAfterUriChange()
+        {
+            // Establish a new connection
+            ConnectionService.Instance.OwnerToConnectionMap.Clear();
+            ConnectionInfo connectionInfo = LiveConnectionHelper.InitLiveConnectionInfo().ConnectionInfo;
+
+            { // check the initial ansi_nulls option value
+                var queryResult = await ExecuteAndVerifyQuery(
+                    "SELECT ansi_nulls FROM sys.dm_exec_sessions WHERE session_id=@@SPID;",
+                    connectionInfo.OwnerUri);
+
+                Assert.AreEqual(queryResult.RowCount, 1);
+                bool ansiNullsOn = (bool)queryResult.Rows[0][0].RawObject;
+                Assert.AreEqual(true, ansiNullsOn);
+            }
+
+            { // change the ansi_nulls value to OFF
+                var queryResult = await ExecuteAndVerifyQuery(
+                    "SET ANSI_NULLS OFF; SELECT ansi_nulls FROM sys.dm_exec_sessions WHERE session_id=@@SPID;",
+                    connectionInfo.OwnerUri);
+
+                Assert.AreEqual(queryResult.RowCount, 1);
+                bool ansiNullsOn = (bool)queryResult.Rows[0][0].RawObject;
+                Assert.AreEqual(false, ansiNullsOn);
+            }
+
+            string newOwnerUri = "renamed_" + connectionInfo.OwnerUri + "_renamed";
+            var executeParams = new ConnectionUriChangedParams
+            {
+                OriginalOwnerUri = connectionInfo.OwnerUri,
+                NewOwnerUri = newOwnerUri
+            };
+
+            // change the connection URI
+            var requestContext = new Mock<EventContext>();
+            await QueryExecutionService.Instance.HandleConnectionUriChangedNotification(executeParams, requestContext.Object);
+
+            { // rerun the query with new URI and confirm previous option persists
+                var queryResult = await ExecuteAndVerifyQuery(
+                    "SELECT ansi_nulls FROM sys.dm_exec_sessions WHERE session_id=@@SPID;",
+                    newOwnerUri);
+
+                Assert.AreEqual(queryResult.RowCount, 1);
+                bool ansiNullsOn = (bool)queryResult.Rows[0][0].RawObject;
+                Assert.AreEqual(false, ansiNullsOn);
+            }
+        }
+
         private async Task<ResultSetSubset> ExecuteAndVerifyQuery(string query, string ownerUri)
         {
             var requestContext = new Mock<RequestContext<ExecuteRequestResult>>();
             ManualResetEvent sendResultEvent = new ManualResetEvent(false);
-            ExecuteRequestResult result = null;
+            ExecuteRequestResult? result = null;
             requestContext.Setup(x => x.SendResult(It.IsAny<ExecuteRequestResult>()))
                 .Callback<ExecuteRequestResult>(r =>
                 {
@@ -99,7 +152,7 @@ namespace Microsoft.SqlTools.ServiceLayer.IntegrationTests.QueryExecution
             };
 
             var subsetRequestContext = new Mock<RequestContext<SubsetResult>>();
-            SubsetResult subsetResult = null;
+            SubsetResult? subsetResult = null;
             subsetRequestContext.Setup(x => x.SendResult(It.IsAny<SubsetResult>()))
                 .Callback<SubsetResult>(r =>
                 {
