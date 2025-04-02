fd42784804604564781f0e356d79e56eb761fa52
Aasim Khan <aasimkhan30@gmail.com>
2025-04-01 22:31:10 -0700

    Refactor SqlProjectsService: update CreateSqlProjectParams and TargetPlatform utility references

--- src/Microsoft.SqlTools.ServiceLayer/SqlProjects/SqlProjectsService.cs
+++ src/Microsoft.SqlTools.ServiceLayer/SqlProjects/SqlProjectsService.cs
@@ -7,13 +7,11 @@ using System;
 using System.Collections.Concurrent;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.Data.Tools.Schema.SchemaModel;
 using Microsoft.SqlServer.Dac.Projects;
 using Microsoft.SqlTools.Hosting.Protocol;
 using Microsoft.SqlTools.ServiceLayer.Hosting;
 using Microsoft.SqlTools.ServiceLayer.SqlProjects.Contracts;
 using Microsoft.SqlTools.ServiceLayer.Utility;
-using CreateSqlProjectParams = Microsoft.SqlServer.Dac.Projects.CreateSqlProjectParams;
 
 namespace Microsoft.SqlTools.ServiceLayer.SqlProjects
 {
@@ -119,10 +117,10 @@ namespace Microsoft.SqlTools.ServiceLayer.SqlProjects
         {
             await RunWithErrorHandling(async () =>
             {
-                CreateSqlProjectParams createParams = new()
+                SqlServer.Dac.Projects.CreateSqlProjectParams createParams = new()
                 {
                     ProjectType = requestParams.SqlProjectType,
-                    TargetPlatform = requestParams.DatabaseSchemaProvider == null ? null : SqlPlatformsUtil.DatabaseSchemaProviderToSqlPlatform(requestParams.DatabaseSchemaProvider),
+                    TargetPlatform = requestParams.DatabaseSchemaProvider == null ? null : Utilities.DatabaseSchemaProviderToSqlPlatform(requestParams.DatabaseSchemaProvider),
                     BuildSdkVersion = requestParams.BuildSdkVersion
                 };
 
@@ -178,7 +176,7 @@ namespace Microsoft.SqlTools.ServiceLayer.SqlProjects
 
         internal async Task HandleSetDatabaseSchemaProviderRequest(SetDatabaseSchemaProviderParams requestParams, RequestContext<ResultStatus> requestContext)
         {
-           await RunWithErrorHandling(() => GetProject(requestParams.ProjectUri, onlyLoadProperties: true).Properties.TargetSqlPlatform = SqlPlatformsUtil.DatabaseSchemaProviderToSqlPlatform(requestParams.DatabaseSchemaProvider), requestContext);
+            await RunWithErrorHandling(() => GetProject(requestParams.ProjectUri, onlyLoadProperties: true).Properties.TargetSqlPlatform = Utilities.DatabaseSchemaProviderToSqlPlatform(requestParams.DatabaseSchemaProvider), requestContext);
         }
 
         #endregion
