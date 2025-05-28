35a1ce5d5ad4347a4ed6111655905f47ca9b2ee3
Aasim Khan <aasimkhan30@gmail.com>
2025-05-28 15:24:22 -0700

    Consolidate async request handler.

--- src/Microsoft.SqlTools.ServiceLayer/SchemaDesigner/SchemaDesignerService.cs
+++ src/Microsoft.SqlTools.ServiceLayer/SchemaDesigner/SchemaDesignerService.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.SqlTools.Hosting.Protocol;
+using Microsoft.SqlTools.ServiceLayer.Management;
 using Microsoft.SqlTools.Utility;
 
 namespace Microsoft.SqlTools.ServiceLayer.SchemaDesigner
@@ -41,7 +42,7 @@ namespace Microsoft.SqlTools.ServiceLayer.SchemaDesigner
 
         internal Task HandleGetSchemaModelRequest(CreateSessionRequest requestParams, RequestContext<CreateSessionResponse> requestContext)
         {
-            return this.HandleRequest<CreateSessionResponse>(requestContext, async () =>
+            return Utils.HandleRequest<CreateSessionResponse>(requestContext, async () =>
             {
                 string connectionUri = Guid.NewGuid().ToString();
                 var session = new SchemaDesignerSession(requestParams.ConnectionString, requestParams.AccessToken);
@@ -60,7 +61,7 @@ namespace Microsoft.SqlTools.ServiceLayer.SchemaDesigner
 
         internal Task HandleGetSchemaDesignerScriptRequest(GenerateScriptRequest requestParams, RequestContext<GenerateScriptResponse> requestContext)
         {
-            return this.HandleRequest<GenerateScriptResponse>(requestContext, async () =>
+            return Utils.HandleRequest<GenerateScriptResponse>(requestContext, async () =>
             {
                 await requestContext.SendResult(new GenerateScriptResponse()
                 {
@@ -73,7 +74,7 @@ namespace Microsoft.SqlTools.ServiceLayer.SchemaDesigner
 
         internal Task HandleDisposeSchemaDesignerSessionRequest(DisposeSessionRequest requestParams, RequestContext<DisposeSessionResponse> requestContext)
         {
-            return this.HandleRequest<DisposeSessionResponse>(requestContext, async () =>
+            return Utils.HandleRequest<DisposeSessionResponse>(requestContext, async () =>
             {
 
                 try
@@ -95,7 +96,7 @@ namespace Microsoft.SqlTools.ServiceLayer.SchemaDesigner
 
         internal Task HandlePublishSchemaDesignerSessionRequest(PublishSessionRequest requestParams, RequestContext<PublishSessionResponse> requestContext)
         {
-            return this.HandleRequest<PublishSessionResponse>(requestContext, async () =>
+            return Utils.HandleRequest<PublishSessionResponse>(requestContext, async () =>
             {
                 if (sessions.TryGetValue(requestParams.SessionId, out SchemaDesignerSession? session))
                 {
@@ -108,31 +109,12 @@ namespace Microsoft.SqlTools.ServiceLayer.SchemaDesigner
 
         internal Task HandleGetSchemaDesignerSessionReportRequest(GetReportRequest requestParams, RequestContext<GetReportResponse> requestContext)
         {
-            return this.HandleRequest<GetReportResponse>(requestContext, async () =>
+            return Utils.HandleRequest<GetReportResponse>(requestContext, async () =>
             {
                 SchemaDesignerSession session = sessions[requestParams.SessionId];
                 var report = await session.GetReport(requestParams.UpdatedSchema);
                 await requestContext.SendResult(report);
             });
-        }
-
-        private Task HandleRequest<T>(RequestContext<T> requestContext, Func<Task> action)
-        {
-            // The request handling will take some time to return, we need to use a separate task to run the request handler so that it won't block the main thread.
-            // For any specific table designer instance, ADS UI can make sure there are at most one request being processed at any given time, so we don't have to worry about race conditions.
-            Task.Run(async () =>
-            {
-                try
-                {
-                    await action();
-                }
-                catch (Exception e)
-                {
-                    Logger.Error(e.Message);
-                    await requestContext.SendError(e);
-                }
-            });
-            return Task.CompletedTask;
         }
     }
 }
