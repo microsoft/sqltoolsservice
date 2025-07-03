f402ef0c74788729b10778fbf833b557414971f9
Aasim Khan <aaskhan@microsoft.com>
2025-07-03 00:22:08 -0700

    Adding back whitespace

--- src/Microsoft.SqlTools.ServiceLayer/LanguageServices/LanguageService.cs
+++ src/Microsoft.SqlTools.ServiceLayer/LanguageServices/LanguageService.cs
@@ -1664,6 +1664,7 @@ namespace Microsoft.SqlTools.ServiceLayer.LanguageServices
             // cache the current script parse info object to resolve completions later
             this.currentCompletionParseInfo = scriptParseInfo;
             resultCompletionItems = result.CompletionItems;
+            
             /*
              Expanding star expressions in query only when the script is connected to a database
              as the parser requires a connection to determine column names
