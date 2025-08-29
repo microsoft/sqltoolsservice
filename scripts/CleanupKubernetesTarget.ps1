adc98a9fcf28f04394f6259e67b66d315579edef
Karl Burtram <karlb@microsoft.com>
2025-08-29 08:41:21 -0700

    Remove scripts Kubernetes files (#2517)

--- scripts/CleanupKubernetesTarget.ps1
+++
@@ -1,9 +0,0 @@
-﻿Param (
-    $svcName = $env:k8ServiceName
-)
-$env:KUBECONFIG = $env:AGENT_WORKFOLDER + "\kubeconfig.centralus.json"
-
-$kube = $env:AGENT_WORKFOLDER + "\kubectl.exe"
-
-iex "$kube delete service $svcName"
-iex "$kube delete deployment $svcName"
