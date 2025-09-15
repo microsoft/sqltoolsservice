76c82fd3b67e4f7b8ce983a896bac8da151521d4
Lewis Sanchez <lewissanchez@microsoft.com>
2025-09-15 15:34:56 -0700

    Adds tests for ScriptEdits method

--- test/Microsoft.SqlTools.ServiceLayer.UnitTests/EditData/SessionTests.cs
+++ test/Microsoft.SqlTools.ServiceLayer.UnitTests/EditData/SessionTests.cs
@@ -1020,6 +1020,83 @@ namespace Microsoft.SqlTools.ServiceLayer.UnitTests.EditData
             }
         }
 
+        [Test]
+        public void ScriptEditsNoParamNotInitialized()
+        {
+            // Setup:
+            //      Create a session without initializing
+            var emf = new Mock<IEditMetadataFactory>();
+            var s = new EditSession(emf.Object);
+
+            // If: I ask to script edits without initializing
+            // Then: I should get an exception
+            Assert.Throws<InvalidOperationException>(() => s.ScriptEdits());
+        }
+
+        [Test]
+        public async Task ScriptEditsNoParamNoEdits()
+        {
+            // Setup: Create a session with a proper query and metadata
+            EditSession s = await GetBasicSession();
+
+            // If: I script the edit cache with no edits
+            string[] scripts = s.ScriptEdits();
+
+            // Then: The script should be empty
+            Assert.That(scripts, Is.Empty);
+        }
+
+        [Test]
+        public async Task ScriptEditsNoParamSingleEdit()
+        {
+            // Setup:
+            //      Create a session with a proper query and metadata
+            EditSession s = await GetBasicSession();
+
+            // ... Add one mock edit that will generate a script
+            var edit = new Mock<RowEditBase>();
+            edit.Setup(e => e.GetScript()).Returns("INSERT statement");
+            s.EditCache[0] = edit.Object;
+
+            // If: I script the edit cache
+            string[] scripts = s.ScriptEdits();
+
+            // Then:
+            // ... The script should contain the edit script
+            Assert.That(scripts, Does.Contain("INSERT statement"));
+        }
+
+        [Test]
+        public async Task ScriptEditsNoParamMultipleEdits()
+        {
+            // Setup:
+            //      Create a session with a proper query and metadata
+            EditSession s = await GetBasicSession();
+
+            // ... Add multiple mock edits that will generate scripts
+            var insert = new Mock<RowEditBase>();
+            insert.Setup(e => e.GetScript()).Returns("INSERT statement");
+
+            var update = new Mock<RowEditBase>();
+            update.Setup(e => e.GetScript()).Returns("UPDATE statement");
+
+            var delete = new Mock<RowEditBase>();
+            delete.Setup(e => e.GetScript()).Returns("DELETE statement");
+
+            s.EditCache[0] = insert.Object;
+            s.EditCache[1] = update.Object;
+            s.EditCache[2] = delete.Object;
+
+            // If: I script the edit cache
+            string[] scripts = s.ScriptEdits();
+
+            // Then:
+            // ... The script should contain all edit scripts
+            Assert.That(scripts, Does.Contain("INSERT statement"));
+            Assert.That(scripts, Does.Contain("UPDATE statement"));
+            Assert.That(scripts, Does.Contain("DELETE statement"));
+        }
+
         #endregion
 
         #region Commit Tests
