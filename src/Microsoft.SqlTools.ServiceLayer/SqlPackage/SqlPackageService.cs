7e43cb1b533958e7701534c2cf3cba79732685fa
Sai Avishkar Sreerama <ssreerama@microsoft.com>
2025-12-08 16:54:58 -0600

    adding hint path to use the hybrid package and updated sqlpakcage service

--- src/Microsoft.SqlTools.ServiceLayer/SqlPackage/SqlPackageService.cs
+++ src/Microsoft.SqlTools.ServiceLayer/SqlPackage/SqlPackageService.cs
@@ -4,7 +4,6 @@
 //
 
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Data.Tools.Schema.CommandLineTool;
 using Microsoft.SqlServer.Dac;
@@ -37,47 +36,66 @@ namespace Microsoft.SqlTools.ServiceLayer.SqlPackage
         /// <param name="serviceHost"></param>
         public void InitializeService(ServiceHost serviceHost)
         {
-            serviceHost.SetRequestHandler(GeneratePublishCommandRequest.Type, this.HandleGeneratePublishCommandRequest, true);
+            serviceHost.SetRequestHandler(GenerateSqlPackageCommandRequest.Type, this.HandleGenerateSqlPackageCommandRequest, true);
         }
 
         /// <summary>
-        /// Handles request to generate a SqlPackage publish command string
+        /// Handles request to generate a SqlPackage command string based on action
         /// </summary>
-        public async Task HandleGeneratePublishCommandRequest(GeneratePublishCommandParams parameters, RequestContext<GeneratePublishCommandResult> requestContext)
+        public async Task HandleGenerateSqlPackageCommandRequest(GenerateSqlPackageCommandParams parameters, RequestContext<SqlPackageCommandResult> requestContext)
         {
             try
             {
-                // Convert DeploymentOptions from VS Code to DacDeployOptions
-                DacDeployOptions? dacOptions = null;
-                if (parameters.DeploymentOptions != null)
+                string command;
+                string action = parameters.Action?.ToLowerInvariant() ?? string.Empty;
+
+                switch (action)
                 {
-                    dacOptions = DacFxUtils.CreateDeploymentOptions(parameters.DeploymentOptions);
+                    case "publish":
+                        command = GeneratePublishCommand(parameters);
+                        break;
+
+                    case "extract":
+                        command = GenerateExtractCommand(parameters);
+                        break;
+
+                    case "script":
+                        command = GenerateScriptCommand(parameters);
+                        break;
+
+                    case "export":
+                        command = GenerateExportCommand(parameters);
+                        break;
+
+                    case "import":
+                        command = GenerateImportCommand(parameters);
+                        break;
+
+                    default:
+                        throw new ArgumentException($"Unsupported action: {parameters.Action}. Supported actions are: Publish, Extract, Script, Export, Import");
                 }
 
-                // Generate SqlPackage command string
-                string commandString = GeneratePublishCommand(
-                    sourceFile: parameters.SourceFile,
-                    targetFile: parameters.TargetFile,
-                    targetConnectionString: parameters.TargetConnectionString,
-                    targetServerName: parameters.TargetServerName,
-                    targetDatabaseName: parameters.TargetDatabaseName,
-                    profilePath: parameters.ProfilePath,
-                    deployOptions: dacOptions,
-                    variables: parameters.Variables,
-                    includeExecutableName: parameters.IncludeExecutableName
-                );
-
-                await requestContext.SendResult(new GeneratePublishCommandResult()
+                // Add or remove executable name based on parameter
+                if (!parameters.IncludeExecutableName && command.StartsWith("sqlpackage ", StringComparison.OrdinalIgnoreCase))
                 {
-                    Command = commandString,
+                    command = command.Substring("sqlpackage ".Length);
+                }
+                else if (parameters.IncludeExecutableName && !command.StartsWith("sqlpackage ", StringComparison.OrdinalIgnoreCase))
+                {
+                    command = "sqlpackage " + command;
+                }
+
+                await requestContext.SendResult(new SqlPackageCommandResult()
+                {
+                    Command = command,
                     Success = true,
                     ErrorMessage = string.Empty
                 });
             }
             catch (Exception e)
             {
-                Logger.Error($"SqlPackage GeneratePublishCommand failed: {e.Message}");
-                await requestContext.SendResult(new GeneratePublishCommandResult()
+                Logger.Error($"SqlPackage Generate{parameters.Action}Command failed: {e.Message}");
+                await requestContext.SendResult(new SqlPackageCommandResult()
                 {
                     Command = null,
                     Success = false,
@@ -86,43 +104,78 @@ namespace Microsoft.SqlTools.ServiceLayer.SqlPackage
             }
         }
 
-        /// <summary>
-        /// Generates a SqlPackage.exe publish command string from the provided parameters
-        /// Uses Microsoft.SqlPackage NuGet API (SqlPackageCommandBuilder.GeneratePublishCommand)
-        /// </summary>
-        private string GeneratePublishCommand(
-            string sourceFile,
-            string targetFile = null,
-            string targetConnectionString = null,
-            string targetServerName = null,
-            string targetDatabaseName = null,
-            string profilePath = null,
-            DacDeployOptions deployOptions = null,
-            Dictionary<string, string> variables = null,
-            bool includeExecutableName = true)
+        private string GeneratePublishCommand(GenerateSqlPackageCommandParams parameters)
         {
-            // Use SqlPackage NuGet API to generate the command
-            string command = SqlPackageCommandBuilder.GeneratePublishCommand(
-                sourceFile: sourceFile,
-                targetConnectionString: targetConnectionString,
-                targetServerName: targetServerName,
-                targetDatabaseName: targetDatabaseName,
-                profilePath: profilePath,
-                deployOptions: deployOptions,
-                variables: variables
+            DacDeployOptions? dacOptions = null;
+            if (parameters.DeploymentOptions != null)
+            {
+                dacOptions = DacFxUtils.CreateDeploymentOptions(parameters.DeploymentOptions);
+            }
+
+            return SqlPackageCommandBuilder.GeneratePublishCommand(
+                sourceFile: parameters.SourceFile,
+                targetConnectionString: parameters.TargetConnectionString,
+                targetServerName: parameters.TargetServerName,
+                targetDatabaseName: parameters.TargetDatabaseName,
+                profilePath: parameters.ProfilePath,
+                deployOptions: dacOptions,
+                variables: parameters.Variables
             );
+        }
 
-            // Add or remove executable name based on parameter
-            if (!includeExecutableName && command.StartsWith("sqlpackage ", StringComparison.OrdinalIgnoreCase))
+        private string GenerateExtractCommand(GenerateSqlPackageCommandParams parameters)
+        {
+            return SqlPackageCommandBuilder.GenerateExtractCommand(
+                sourceConnectionString: parameters.SourceConnectionString,
+                targetFile: parameters.TargetFile,
+                sourceServerName: parameters.SourceServerName,
+                sourceDatabaseName: parameters.SourceDatabaseName,
+                applicationName: parameters.ApplicationName,
+                applicationVersion: parameters.ApplicationVersion,
+                properties: parameters.Properties
+            );
+        }
+
+        private string GenerateScriptCommand(GenerateSqlPackageCommandParams parameters)
+        {
+            DacDeployOptions? dacOptions = null;
+            if (parameters.DeploymentOptions != null)
             {
-                command = command.Substring("sqlpackage ".Length);
-            }
-            else if (includeExecutableName && !command.StartsWith("sqlpackage ", StringComparison.OrdinalIgnoreCase))
-            {
-                command = "sqlpackage " + command;
+                dacOptions = DacFxUtils.CreateDeploymentOptions(parameters.DeploymentOptions);
             }
 
-            return command;
+            return SqlPackageCommandBuilder.GenerateScriptCommand(
+                sourceFile: parameters.SourceFile,
+                targetFile: parameters.TargetFile,
+                targetConnectionString: parameters.TargetConnectionString,
+                targetServerName: parameters.TargetServerName,
+                targetDatabaseName: parameters.TargetDatabaseName,
+                profilePath: parameters.ProfilePath,
+                deployOptions: dacOptions,
+                variables: parameters.Variables
+            );
+        }
+
+        private string GenerateExportCommand(GenerateSqlPackageCommandParams parameters)
+        {
+            return SqlPackageCommandBuilder.GenerateExportCommand(
+                sourceConnectionString: parameters.SourceConnectionString,
+                targetFile: parameters.TargetFile,
+                sourceServerName: parameters.SourceServerName,
+                sourceDatabaseName: parameters.SourceDatabaseName,
+                properties: parameters.Properties
+            );
+        }
+
+        private string GenerateImportCommand(GenerateSqlPackageCommandParams parameters)
+        {
+            return SqlPackageCommandBuilder.GenerateImportCommand(
+                sourceFile: parameters.SourceFile,
+                targetConnectionString: parameters.TargetConnectionString,
+                targetServerName: parameters.TargetServerName,
+                targetDatabaseName: parameters.TargetDatabaseName,
+                properties: parameters.Properties
+            );
         }
     }
 }
