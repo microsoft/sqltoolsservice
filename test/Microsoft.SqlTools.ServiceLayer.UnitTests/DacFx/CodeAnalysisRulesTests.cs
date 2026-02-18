4f1ef164780ffa409e8c0675ed99b4e13bf59257
Sai Avishkar Sreerama <ssreerama@microsoft.com>
2026-02-18 14:50:42 -0600

    cleanup

--- test/Microsoft.SqlTools.ServiceLayer.UnitTests/DacFx/CodeAnalysisRulesTests.cs
+++ test/Microsoft.SqlTools.ServiceLayer.UnitTests/DacFx/CodeAnalysisRulesTests.cs
@@ -3,8 +3,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 //
 
-#nullable disable
-
 using System.Linq;
 using Microsoft.SqlServer.Dac.CodeAnalysis;
 using Microsoft.SqlServer.Dac.Model;
