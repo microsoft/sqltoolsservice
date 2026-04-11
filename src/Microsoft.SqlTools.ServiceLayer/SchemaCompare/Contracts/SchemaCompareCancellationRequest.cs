18b41245bd310a80a31562ec3ce9aa9e3842d053
bruzel <bruzel@microsoft.com>
2026-04-11 22:15:47 +0200

    Update schema compare cancellation request class

--- src/Microsoft.SqlTools.ServiceLayer/SchemaCompare/Contracts/SchemaCompareCancellationRequest.cs
+++ src/Microsoft.SqlTools.ServiceLayer/SchemaCompare/Contracts/SchemaCompareCancellationRequest.cs
@@ -1,24 +1,20 @@
-﻿//
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-//
 
 #nullable disable
 
 using Microsoft.SqlTools.Hosting.Protocol.Contracts;
 using Microsoft.SqlTools.ServiceLayer.Utility;
+using CoreContracts = Microsoft.SqlTools.SqlCore.SchemaCompare.Contracts;
 
 namespace Microsoft.SqlTools.ServiceLayer.SchemaCompare.Contracts
 {
     /// <summary>
-    /// Parameters for a schema compare cancel request
+    /// Parameters for a schema compare cancel request.
+    /// Extends CoreContracts version - no additional ServiceLayer-specific fields needed.
     /// </summary>
-    public class SchemaCompareCancelParams
+    public class SchemaCompareCancelParams : CoreContracts.SchemaCompareCancelParams
     {
-        /// <summary>
-        /// Operation id of the schema compare operation
-        /// </summary>
-        public string OperationId { get; set; }
     }
 
     /// <summary>
