3f489d00d0bdb49094f93519cf8c2e54485806b4
bruzel <bruzel@microsoft.com>
2026-04-11 22:17:08 +0200

    refactor: SchemaCompareSaveScmpParams chains through ServiceLayer.SchemaCompareParams - removes duplicate fields

--- src/Microsoft.SqlTools.ServiceLayer/SchemaCompare/Contracts/SchemaCompareSaveScmpRequest.cs
+++ src/Microsoft.SqlTools.ServiceLayer/SchemaCompare/Contracts/SchemaCompareSaveScmpRequest.cs
@@ -1,7 +1,5 @@
-﻿//
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-//
 
 #nullable disable
 
@@ -11,6 +9,13 @@ using Microsoft.SqlTools.SqlCore.SchemaCompare.Contracts;
 
 namespace Microsoft.SqlTools.ServiceLayer.SchemaCompare.Contracts
 {
+    /// <summary>
+    /// Parameters for a schema compare save SCMP request.
+    /// Extends ServiceLayer.SchemaCompareParams (which itself extends CoreContracts.SchemaCompareParams),
+    /// picking up TaskExecutionMode through the chain. ScmpFilePath and excluded objects are
+    /// defined here; all other fields (OperationId, Source/TargetEndpointInfo, DeploymentOptions)
+    /// come from the base classes.
+    /// </summary>
     internal class SchemaCompareSaveScmpParams : SchemaCompareParams
     {
         /// <summary>
@@ -32,7 +37,6 @@ namespace Microsoft.SqlTools.ServiceLayer.SchemaCompare.Contracts
     internal class SchemaCompareSaveScmpRequest
     {
         public static readonly RequestType<SchemaCompareSaveScmpParams, ResultStatus> Type =
-    RequestType<SchemaCompareSaveScmpParams, ResultStatus>.Create("schemaCompare/saveScmp");
+            RequestType<SchemaCompareSaveScmpParams, ResultStatus>.Create("schemaCompare/saveScmp");
     }
-
 }
