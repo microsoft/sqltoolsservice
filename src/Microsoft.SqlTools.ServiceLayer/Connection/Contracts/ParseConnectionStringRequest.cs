5386a584a53debbdd8bf52c3623672428855189f
Aasim Khan <aaskhan@microsoft.com>
2026-06-24 22:20:04 -0700

    Use object params for parse connection string

--- src/Microsoft.SqlTools.ServiceLayer/Connection/Contracts/ParseConnectionStringRequest.cs
+++ src/Microsoft.SqlTools.ServiceLayer/Connection/Contracts/ParseConnectionStringRequest.cs
@@ -10,12 +10,23 @@ using Microsoft.SqlTools.Hosting.Protocol.Contracts;
 namespace Microsoft.SqlTools.ServiceLayer.Connection.Contracts
 {
     /// <summary>
+    /// Parameters for the Parse Connection String Request.
+    /// </summary>
+    public class ParseConnectionStringParams
+    {
+        /// <summary>
+        /// Connection string to parse into connection details.
+        /// </summary>
+        public string ConnectionString { get; set; }
+    }
+
+    /// <summary>
     /// Parse Connection String request
     /// </summary>
     internal class ParseConnectionStringRequest
     {
         public static readonly
-            RequestType<string, ConnectionDetails> Type =
-            RequestType<string, ConnectionDetails>.Create("connection/parseConnectionString");
+            RequestType<ParseConnectionStringParams, ConnectionDetails> Type =
+            RequestType<ParseConnectionStringParams, ConnectionDetails>.Create("connection/parseConnectionString");
     }
 }
--- test/Microsoft.SqlTools.ServiceLayer.IntegrationTests/Connection/ConnectionServiceTests.cs
+++ test/Microsoft.SqlTools.ServiceLayer.IntegrationTests/Connection/ConnectionServiceTests.cs
@@ -217,7 +217,10 @@ namespace Microsoft.SqlTools.ServiceLayer.IntegrationTests.Connection
             var requestContext = RequestContextMocks.Create<ConnectionDetails>(r => { result = r; resultSent = true; }).AddErrorHandling((msg, code, data) => { error = msg; errorSent = true; });
 
             // Validate successful parse
-            await service.HandleParseConnectionStringRequest("Server=tcp:{servername},1433;Initial Catalog={databasename};Authentication=ActiveDirectoryInteractive;", requestContext.Object);
+            await service.HandleParseConnectionStringRequest(new ParseConnectionStringParams
+            {
+                ConnectionString = "Server=tcp:{servername},1433;Initial Catalog={databasename};Authentication=ActiveDirectoryInteractive;"
+            }, requestContext.Object);
 
             Assert.That(result.ServerName, Is.EqualTo("tcp:{servername},1433"), "Valid connection string should return parsed ConnectionDetails object");
             Assert.That(errorSent, Is.False, "Valid connection string should not return an error");
@@ -229,7 +232,10 @@ namespace Microsoft.SqlTools.ServiceLayer.IntegrationTests.Connection
             error = null;
             errorSent = false;
 
-            await service.HandleParseConnectionStringRequest("Server=tcp:{servername},1433;Initial Catalog={databasename};Authentication=NotRealAuthType;", requestContext.Object);
+            await service.HandleParseConnectionStringRequest(new ParseConnectionStringParams
+            {
+                ConnectionString = "Server=tcp:{servername},1433;Initial Catalog={databasename};Authentication=NotRealAuthType;"
+            }, requestContext.Object);
 
             Assert.That(result, Is.Null, "Invalid connection string should not return ConnectionDetails");
             Assert.That(resultSent, Is.False, "Invalid connection string should not return anything");
